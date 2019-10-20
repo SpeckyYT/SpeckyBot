05ea1c6199e342ce410ac4654de0b3d6bbab6ba9
SpeckyYT <Specky.fans@gmail.com>
2019-10-20 22:18:59 +0200

    it works now

--- cmds/owner/leaveserver.js
+++ cmds/owner/leaveserver.js
@@ -4,15 +4,19 @@ module.exports.run = async (bot, msg, args, owner, prefix) => {
         return;
     }
     if(!args[0]) return msg.channel.send("You have to define a server");
-    guild = fetchGuild(args[0]);
-    guild.leave();
-
+    guild = bot.guilds.get(args[0]);
+    try{
+        guild.leave();
+        msg.channel.send("It should have worked!")
+    }catch(e){
+        msg.channel.send("An error occourred")
+    }
 }
 
 module.exports.config = {
     name: "leaveserver",
-	description: "The bot will leave the servers where there isn't the owner!",
-    usage: ``,
+	description: "The bot will leave the desired server!",
+    usage: `<serverID>`,
     category: `owner`,
 	accessableby: "Bot Owner",
     aliases: ["ls","sl","serverleave"]
