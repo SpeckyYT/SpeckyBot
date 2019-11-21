35cf5b7731a0c1872ae34305cc26e5517af1e0a0
SpeckyYT <Specky.fans@gmail.com>
2019-11-21 18:06:27 +0100

    start working on mee6 command

--- commands/misc/mee6.js
+++ commands/misc/mee6.js
@@ -1,55 +1,15 @@
-const mee6 = require('mee6-levels-api')
+const fetch = require('node-fetch')
+const { RichEmbed } = require('discord.js')
 
 module.exports.run = async (bot, msg, args, config) => {
 
-    var serverID, userID;
+    var baseURL = `https://mee6.xyz/api/plugins/levels/leaderboard/`
 
-    if(args[1]) serverID = args[1];
-    if(args[2]) userID = args[2];
-
-    switch(args[0]){
-        case "server":
-        case "servers":
-        case "lb":
-        case "leaderboard":
-            try{
-                mee6.getLeaderboard(serverID).then(leaderboard => {
-                    console.log(leaderboard)
-                    for(const user of leaderboard) msg.channel.send(`${user.tag} - ${user.rank}`);
-                })
-            }catch(e){
-                msg.channel.send("Error occurred [server ID may be wrong]")
-            }
-        break
-
-        case "roles":
-        case "role":
-        case "rolerewards":
-            try{
-                mee6.getRoleRewards(serverID).then(rewards => {
-                    console.log(rewards)
-                    for(const reward of rewards) msg.channel.send(`${reward.role.name} - Given at level **${reward.rank}**`);
-                })
-            }catch(e){
-                msg.channel.send("Error occurred [server ID may be wrong]")
-            }
-        break
-
-        case "xp":
-        case "experience":
-            try{
-                mee6.getUserXP(serverID, userID).then(user => {
-                    console.log(user)
-                    msg.channel.send(`${user.tag} is at level **${user.level}** and at rank **${user.rank}**`);
-                })
-            }catch(e){
-                msg.channel.send("Error occurred [server ID or user ID may be wrong]")
-            }
-        break
-
-        default:
-            msg.channel.send("You have to define an action [server/roles/xp]");
-    }
+    fetch(`${baseURL}${args[0]}`)
+        .then(resp => resp.json())
+        .then(json => {
+            msg.channel.send(json.page)
+        })
 }
 
 module.exports.config = {
