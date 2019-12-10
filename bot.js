3334fc6efb18e6ca1711336bb91e5556fe278a7a
SpeckyYT <Specky.fans@gmail.com>
2019-12-10 17:29:45 +0100

    some more fixes

--- bot.js
+++ bot.js
@@ -16,6 +16,6 @@ mf().then(prom => {if(prom)return});
 
 const { token, prefix } = require("./config.json");
 
-bot.login(token).catch(() => {for(var i = 0; i < 100; i++)console.log('PLEASE EDIT THE CONFIG.JSON FILE')})
+bot.login(token).catch(() => {for(var i = 0; i < 100; i++)console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`)})
 
 console.log(`Bot prefix: ${prefix}`);
