26c56029f24f1e08ec906633baac80f39b7592cc
SpeckyYT <Specky.fans@gmail.com>
2019-12-10 17:06:22 +0100

    small code improvement

--- handlers/missingfiles.js
+++ handlers/missingfiles.js
@@ -4,28 +4,19 @@ module.exports = async () => {
     let missingFiles = false;
 
     if (!existsSync('./config.json')) {
-        await appendFileSync('./config.json','{\n"token": "TOKEN_HERE",\n"prefix": "PREFIX_HERE",\n"owner": "BOTOWNERID_HERE",\n"youtube": "YOUTUBE_V3_KEY_HERE"\n}')
+        appendFileSync('./config.json','{\n"token": "TOKEN_HERE",\n"prefix": "PREFIX_HERE",\n"owner": "BOT_OWNER_ID_HERE",\n"youtube": "YOUTUBE_V3_KEY_HERE"\n}')
         missingFiles = true
     }
-    if (!existsSync('./s_settings.json')) {
-        await appendFileSync('./s_settings.json','{}')
-        missingFiles = true
-    }
-    if (!existsSync('./u_settings.json')) {
-        await appendFileSync('./u_settings.json','{}')
-        missingFiles = true
-    }
-    if (!existsSync('./textchannelcons.txt')) {
-        await appendFileSync('./textchannelcons.txt','')
-        missingFiles = true
-    }
-
-    openSync('./config.json','a', (err,fd) => {
-        if(fd.includes('PLEASE EDIT THE CONFIG.JSON FILE')){
-            console.log('PLEASE EDIT THE CONFIG.JSON FILE')
-            
+    ['s_settings','u_settings'].forEach(file => {
+        if (!existsSync(`./${file}.json`)) {
+            appendFileSync(`./${file}.json`,'{}')
+            missingFiles = true
         }
     })
+    if (!existsSync('./textchannelcons.txt')) {
+        appendFileSync('./textchannelcons.txt','')
+        missingFiles = true
+    }
 
     if(missingFiles){return missingFiles}
 }
