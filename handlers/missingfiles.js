c174c022c3844e6128e49245dce1e301f744eb90
SpeckyYT <Specky.fans@gmail.com>
2019-12-16 16:47:07 +0100

    Update missingfiles.js

--- handlers/missingfiles.js
+++ handlers/missingfiles.js
@@ -4,9 +4,9 @@ module.exports = async () => {
     if (!existsSync('./config.json')) {
         appendFileSync('./config.json','{\n"token": "TOKEN_HERE",\n"prefix": "PREFIX_HERE",\n"owner": "BOT_OWNER_ID_HERE",\n"youtube": "YOUTUBE_V3_KEY_HERE"\n}')
     }
-    ['s_settings','u_settings'].forEach(file => {
-        if (!existsSync(`./${file}.json`)) {
-            appendFileSync(`./${file}.json`,'{}')
+    ['s','u'].forEach(file => {
+        if (!existsSync(`./${file}_settings.json`)) {
+            appendFileSync(`./${file}_settings.json`,'{}')
         }
     })
     if (!existsSync('./textchannelcons.txt')) {
