d906a1e5fcc173128de07a7ff53b5618f973086c
SpeckyYT <Specky.fans@gmail.com>
2019-12-17 14:02:16 +0100

    Uptime fix

--- commands/utilities/uptime.js
+++ commands/utilities/uptime.js
@@ -3,8 +3,8 @@ module.exports.run = async (bot, msg, args, config) => {
         const mil = Math.floor((ms % 1000)).toString();
         const sec = Math.floor((ms / 1000) % 60).toString();
         const min = Math.floor((ms / (1000 * 60)) % 60).toString();
-        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString();
-        const day = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString();
+        const hrs = Math.floor((ms / (1000 * 60   * 60)) % 24).toString();
+        const day = Math.floor((ms / (1000 * 60   * 60   * 24)) % 60).toString();
         return `${day.padStart(1, "0")} days, ${hrs.padStart(2, "0")} hours, ${min.padStart(2, "0")} minutes, ${sec.padStart(1, "0")} seconds, ${mil.padStart(3, "0")} milliseconds.`
     }
 
