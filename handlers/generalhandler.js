7d2b64e5d19688f3e9873c991863a2f0b0706a8a
SpeckyYT <Specky.fans@gmail.com>
2019-12-19 20:10:52 +0100

    Update generalhandler.js

--- handlers/generalhandler.js
+++ handlers/generalhandler.js
@@ -2,15 +2,9 @@ const { Client, Collection } = require("discord.js");
 
 module.exports = () => {
     const bot = new Client({autoReconnect:true});
+
     require('./missingdirectories')();
     require('./missingfiles')();
-    require('./botloader')();
-    
-    const { token } = require('../config.json')
-        
-    bot.login(token).catch(() => {
-        for(var i = 0; i < 50; i++){
-            console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`)
-        }
-    })
+    require('./botloader')(bot);
+    require('./login')(bot);
 }
