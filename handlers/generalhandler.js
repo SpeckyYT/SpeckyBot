e2fd04efb44564754c47f9e561ff2a6c04ef7e8a
SpeckyYT <Specky.fans@gmail.com>
2019-12-19 20:03:09 +0100

    Splitting the handlers a bit more

--- handlers/generalhandler.js
+++ handlers/generalhandler.js
@@ -1,21 +1,10 @@
 const { Client, Collection } = require("discord.js");
 
 module.exports = () => {
-    require('./missingdirectories')()
-    require('./missingfiles')()
-
     const bot = new Client({autoReconnect:true});
-    
-    bot.music = require("discord.js-musicbot-addon");
-    
-    ["events","commands","aliases"].forEach(async x => 
-        bot[x] = new Collection()
-    );
-
-    ["events", "commands", "console","music"].forEach(async x => {
-        console.log(`\n\nLoading ${x.toUpperCase()}!\n`);
-        require(`./${x}`)(bot)
-    });
+    require('./missingdirectories')();
+    require('./missingfiles')();
+    require('./botloader')();
     
     const { token } = require('../config.json')
         
