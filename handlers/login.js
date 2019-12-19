c2be81dcdd32903ed412fadd0d3603802c8e84e8
SpeckyYT <Specky.fans@gmail.com>
2019-12-19 20:09:20 +0100

    Create login.js

---
+++ handlers/login.js
@@ -0,0 +1,9 @@
+module.exports = (bot) => {
+    const { token } = require('../config.json')
+        
+    bot.login(token).catch(() => {
+        for(var i = 0; i < 25; i++){
+            console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`)
+        }
+    })
+}
