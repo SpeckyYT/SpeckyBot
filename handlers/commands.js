3dc7283805a72f7768b6e3d1bb2fcc26ade78337
SpeckyYT <Specky.fans@gmail.com>
2019-12-21 21:58:13 +0100

    Same with commands handler

--- handlers/commands.js
+++ handlers/commands.js
@@ -18,7 +18,7 @@ module.exports = (bot) => {
                     bot.commands.set(pull.config.name, pull);
                     if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
                     console.log(`${dirs}     \t|\t${file}`);
-                }catch{console.log(`ERROR: ${dirs} | ${file}`)}
+                }catch{console.log(`${dirs}     \t|\t${file} ERROR!`)}
             }
         }
     }
