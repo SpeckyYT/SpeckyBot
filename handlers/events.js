06f40b340075f0add5b7d54cc9032e8a719f98dc
SpeckyYT <Specky.fans@gmail.com>
2019-12-21 21:56:59 +0100

    Error haning in event handler

--- handlers/events.js
+++ handlers/events.js
@@ -4,10 +4,12 @@ module.exports = (bot) => {
     const load = dirs => {    
         const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith('.js'));
         for (let file of events) {
-            const evt = require(`../events/${dirs}/${file}`);
-            let eName = evt.config.event;
-            bot.on(eName, evt.bind(null, bot));
-            console.log(`${dirs}\t|\t${file}`);
+            try{
+                const evt = require(`../events/${dirs}/${file}`);
+                let eName = evt.config.event;
+                bot.on(eName, evt.bind(null, bot));
+                console.log(`${dirs}\t|\t${file}`);
+            }catch{console.log(`${dirs}\t|\t${file} ERROR!`)}
         };
     };
     ["client", "custom", "guild","private"].forEach(x => load(x));
