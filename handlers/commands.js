f6529080d1696a660c84b954b1f56095c62cc670
SpeckyYT <Specky.fans@gmail.com>
2019-12-21 21:48:10 +0100

    What about this?

--- handlers/commands.js
+++ handlers/commands.js
@@ -14,7 +14,7 @@ module.exports = (bot) => {
             }
             if(!error){
                 let pull = require(`../commands/${dirs}/${file}`);
-                bot.commands.set(pull.config.name, pull).catch{};
+                bot.commands.set(pull.config.name, pull).catch(()=>{});
                 if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
                 console.log(`${dirs}     \t|\t${file}`);
             }
