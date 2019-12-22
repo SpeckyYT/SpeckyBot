d789e584c28d87800ceda67e6bd6c1c4904e3778
SpeckyYT <Specky.fans@gmail.com>
2019-12-22 07:27:02 +0100

    Update gitupdate.js

--- commands/owner/gitupdate.js
+++ commands/owner/gitupdate.js
@@ -1,3 +1,12 @@
-module.exports.run = async () => {}
+module.exports.run = async () => {
 
-module.exportd.config = {}
+}
+
+module.exports.config = {
+    name: "gitupdate",
+	description: "The bot will update itself to the newest stand!",
+    usage: ``,
+    category: `owner`,
+	accessableby: "Bot Owner",
+    aliases: ["gu"]
+}
