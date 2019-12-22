d8bdc258e49df7f9edd45de139572e548c35642f
SpeckyYT <Specky.fans@gmail.com>
2019-12-22 09:01:44 +0100

    Update npmupdate.js

--- commands/owner/npmupdate.js
+++ commands/owner/npmupdate.js
@@ -1,1 +1,12 @@
+module.exports.run = async () => {
+	
+}
 
+module.exports.config = {
+    name: "npmupdate",
+	description: "The bot will update all packages!",
+    usage: ``,
+    category: `owner`,
+	accessableby: "Bot Owner",
+    aliases: ["nu"]
+}
