b73d4ebed30b05af00e0847b4dad7256f0336f9b
SpeckyYT <Specky.fans@gmail.com>
2019-12-24 16:56:19 +0100

    Removed errors in cmd handler

--- handlers/commands.js
+++ handlers/commands.js
@@ -6,20 +6,17 @@ module.exports = (bot) => {
     var load = dirs => {
         var commands = readdirSync(`./commands/${dirs}/`).filter(d => d.endsWith('.js'));
         for (let file of commands) {
-            let error = false;
             if(boot){
                 try{
                     delete require.cache[require.resolve(`../commands/${dirs}/${file}`)];
-                }catch{error = true}
+                }catch{}
             }
-            if(!error){
-                try{
-                    let pull = require(`../commands/${dirs}/${file}`);
-                    bot.commands.set(pull.config.name, pull);
-                    if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
-                    console.log(`${dirs}     \t|\t${file}`);
-                }catch{console.log(`${dirs}     \t|\t${file} ERROR!`)}
-            }
+            try{
+                let pull = require(`../commands/${dirs}/${file}`);
+                bot.commands.set(pull.config.name, pull);
+                if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
+                console.log(`${dirs}     \t|\t${file}`);
+            }catch{console.log(`${dirs}     \t|\t${file} ERROR!`);
         }
     }
     ["owner", "admin", "utilities", "external", "games", "misc", "music", "private", "custom"].forEach(x => load(x));
