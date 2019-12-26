8f285ee3b32ad804ae0827c83bfd44f3ea2918f2
SpeckyYT <specky.fans@gmail.com>
2019-12-26 16:34:31 +0000

    added cur command

---
+++ commands/admin/channelsunread.js
@@ -0,0 +1,17 @@
+module.exports.run = async (bot, msg, args, config) => {
+    msg.guild.channels.forEach(async channel => {
+        try{
+            channel.send('New').then(msg => msg.delete());
+        }catch{}
+    })
+}
+
+module.exports.config = {
+    name: "channelsunread",
+	description: "Turns every channel in an unread one!",
+    usage: ``,
+    category: `admin`,
+	accessableby: "Server Admins and Moderators",
+    aliases: ["cur"],
+    perms: ['ADMINISTRATOR']
+}
