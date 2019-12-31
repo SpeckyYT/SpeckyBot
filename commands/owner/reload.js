db1c3ca312089f2758f19ede8402429164b47f45
SpeckyYT <Specky.fans@gmail.com>
2019-12-31 21:28:22 +0100

    still trying....

--- commands/owner/reload.js
+++ commands/owner/reload.js
@@ -39,17 +39,33 @@ module.exports.run = async (bot, msg, args, config) => {
             case "git":
             case "repo":
                 
-                nodegit.Repository.open(path.resolve(__dirname, "../../.git"))
+                let repoDir = "../../";
+
+                let repository;
+
+                // Open a repository that needs to be fetched and fast-forwarded
+                nodegit.Repository.open(path.resolve(__dirname, repoDir))
                 .then(function(repo) {
-                    return repo.fetch("origin", {
+                    repository = repo;
+
+                    return repository.fetchAll({
                         callbacks: {
                             credentials: function(url, userName) {
                                 return nodegit.Cred.sshKeyFromAgent(userName);
+                            },
+                            certificateCheck: function() {
+                                return 0;
                             }
                         }
                     });
-                }).done(function() {
-                    console.log("It worked!");
+                })
+                // Now that we're finished fetching, go ahead and merge our local branch
+                // with the new one
+                .then(function() {
+                    return repository.mergeBranches("master", "origin/master");
+                })
+                .done(function() {
+                    console.log("Done!");
                 });
 
                 break
