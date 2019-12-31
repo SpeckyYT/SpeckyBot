c3945f7bcfbec61b5498806865d2fa19c1c746cc
SpeckyYT <Specky.fans@gmail.com>
2019-12-31 15:43:44 +0100

    Update moderators.js

--- commands/utilities/moderators.js
+++ commands/utilities/moderators.js
@@ -14,10 +14,15 @@ module.exports.run = async (bot, msg, args, config) => {
         list[mod.user.presence.status].push(mod.user.username)
     })
 
+    let online = "<:online2:464520569975603200>"
+    let away = "<:away2:464520569862357002>"
+    let dnd = "<:dnd2:464520569560498197>"
+    let offline = "<:offline2:464520569929334784>"
+
     let embed = new RichEmbed()
     .setTitle("__Mods__:")
     .setThumbnail(msg.guild.iconURL)
-    .setDescription();
+    .setDescription(`${online} ${list.online.join(", ")}\n${away} ${list.idle.join(", ")}\n${dnd} ${list.dnd.join(", ")}\n${offline} ${list.offline.join(", ")}`);
 }
 
 module.exports.config = {
