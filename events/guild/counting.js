32e235ad6ef7c496514ee0cbc57ef43671b1058a
SpeckyYT <Specky.fans@gmail.com>
2020-01-01 18:56:22 +0100

    Update counting.js

--- events/guild/counting.js
+++ events/guild/counting.js
@@ -1,12 +1,12 @@
 module.exports = async (bot, msg) => {
     let text = 'Next number: '
-    if(msg.channel.topic.includes(text)){
+    if(msg.channel.topic.startsWith(text)){
         let number = msg.channel.topic.slice(text.length)
         if(!isNaN(number)){
             if(msg.content != number){
                 msg.delete()
             }else{
-                msg.channel.setTopic(`${text}${number + 1}`)
+                msg.channel.setTopic(`${text}${number++}`)
             }
         }
     }
