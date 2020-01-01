87ff5a618aa4c14062f1875c8d7b28a86507d981
SpeckyYT <Specky.fans@gmail.com>
2020-01-01 10:21:17 +0100

    Update members.js

--- commands/owner/members.js
+++ commands/owner/members.js
@@ -2,9 +2,11 @@ const { appendFile, unlink } = require('fs')
 const { Attachment } = require('discord.js')
 
 module.exports.run = async (bot, msg, args, config) => {
+    let members = [];
     msg.guild.members.forEach(async member => {
-        appendFile('./members.txt',member.user.id + "\n", () => {})
+        members.push(member.user.id)
     })
+    appendFile('./members.txt', members.join('\n'), () => {})
     let att = new Attachment('./members.txt',"members.txt")
     await msg.channel.send(att);
     unlink('./members.txt', () => {})
