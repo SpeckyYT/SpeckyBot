c97a1dee0db3bae83cc4c30b3760147f36fc2e44
SpeckyYT <Specky.fans@gmail.com>
2020-01-02 14:07:46 +0100

    Added catch to counting.js

--- events/guild/counting.js
+++ events/guild/counting.js
@@ -8,14 +8,14 @@ module.exports = async (bot, msg) => {
         let number = parseInt(msg.channel.topic.slice(text.length).trim());
         if(!isNaN(number)){
             if(msg.content != number){
-                msg.delete();
+                msg.delete().catch(()=>{})
             }else if(alttrue && number > 1){
                 msg.channel.fetchMessages({ limit: 2 })
                 .then(msgs => {
                     if(msgs.filter(ms => ms.author.id == msg.author.id).size < 2){
                         msg.channel.setTopic(`${text} ${number + 1} ${alt.toUpperCase()}`);
                     }else{
-                        msg.delete();
+                        msg.delete().catch(()=>{})
                     }
                 })
             }else{
