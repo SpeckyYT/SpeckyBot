f9bc9414de5a6b51ad49bd6de46d9577553807e4
SpeckyYT <Specky.fans@gmail.com>
2020-01-02 18:54:08 +0100

    Create snipe.js

---
+++ commands/utilities/snipe.js
@@ -0,0 +1,22 @@
+const { RichEmbed } = require('discord.js')
+
+module.exports.run = async (bot, msg, args, config) => {
+    msg.channel.fetchMessages({ limit: 2 })
+    .then(msgs => {
+        let ms = msgs.first();
+        let embed = new RichEmbed()
+            .setAuthor(ms.author.username, ms.author.avatarURL)
+            .setColor('000000')
+            .setDescription(ms.content);
+        msg.channel.send(embed);
+    })
+}
+
+module.exports.config = {
+    name: "snipe",
+	description: "Snipes the previous message!",
+    usage: ``,
+    category: `utilities`,
+	accessableby: "Members",
+    aliases: []
+}
