20e460561ed0c82f3dbdaa3618f99862b148a30a
SpeckyYT <Specky.fans@gmail.com>
2020-01-02 13:35:46 +0100

    Update counting.js

--- events/guild/counting.js
+++ events/guild/counting.js
@@ -1,14 +1,25 @@
 module.exports = async (bot, msg) => {
     if(!msg.channel.topic) return;
-    let text = 'Next number:'
     if(msg.channel.topic.startsWith(text)){
         if(msg.author.bot) return msg.delete();
-        let number = msg.channel.topic.slice(text.length).trim();
+        let text = 'Next number:'
+        let alt = '[alternate]'
+        let alttrue = msg.channel.topic.toLowerCase().includes(alt);
+        let number = parseInt(msg.channel.topic.slice(text.length).trim());
         if(!isNaN(number)){
             if(msg.content != number){
                 msg.delete();
+            }else if(alttrue){
+                msg.channel.fetchMessages({ limit: 2 })
+                .then(msgs => {
+                    if(msgs.filter(ms => ms.author.id == msg.author.id).size < 2){
+                        msg.channel.setTopic(`${text} ${number + 1} ${alt.toUpperCase()}`);
+                    }else{
+                        msg.delete();
+                    }
+                })
             }else{
-                msg.channel.setTopic(`${text} ${parseInt(number,10) + 1}`);
+                msg.channel.setTopic(`${text} ${number + 1}`);
             }
         }
     }
