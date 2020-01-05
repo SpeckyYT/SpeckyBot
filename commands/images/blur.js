5b8dbdf1a6aaec3f2dae3f24610ae61862e7f424
SpeckyYT <Specky.fans@gmail.com>
2020-01-05 20:52:53 +0100

    Higher blur possible

--- commands/images/blur.js
+++ commands/images/blur.js
@@ -1,11 +1,11 @@
 module.exports.run = async (bot, msg) => {
-    require('./functions/intensity')(bot, msg,'blur',false,[5,2,50],"png")
+    require('./functions/intensity')(bot, msg,'blur',false,[5,2,100],"png")
 }
 
 module.exports.config = {
     name: "blur",
 	description: "Blurs the image!",
-    usage: `[Amount (2-50)]`,
+    usage: `[Amount (2-100)]`,
     category: `images`,
 	accessableby: "Members",
     aliases: [],
