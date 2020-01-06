9c0bad508d7d5a02fcd8383f530110f6a6635dd9
SpeckyYT <Specky.fans@gmail.com>
2020-01-06 16:47:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-const m3u8stream = require('m3u8stream');
-
-const parseTime = require('m3u8stream/dist/parse-time');
-
 require('./handlers/generalhandler')();
 
 const { prefix } = require("./config.json");
