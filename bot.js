014714b4e88c6796c680c3dedfe464081e0058e4
SpeckyYT <Specky.fans@gmail.com>
2020-01-06 09:29:43 +0100

    Test

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+const m3u8stream = require('m3u8stream');
+
+const parseTime = require('m3u8stream/dist/parse-time');
+
 require('./handlers/generalhandler')();
 
 const { prefix } = require("./config.json");
