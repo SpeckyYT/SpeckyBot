b9e9a39e4cf4e2f6b59a4e1c653dd9cd770e8fef
SpeckyYT <Specky.fans@gmail.com>
2020-01-06 08:48:32 +0100

    Create real.js

---
+++ commands/images/real.js
@@ -0,0 +1,17 @@
+module.exports.run = async (bot, msg) => {
+    if(msg.args){
+        msg.args[0] = Number(ms) / 100
+    }
+    require('./functions/intensity')(bot, msg,'real',false,[0.5,-1,1],"png")
+}
+
+module.exports.config = {
+    name: "contrast",
+	description: "Applies a contrast to the image!",
+    usage: `[Amount (-100 - 100)]`,
+    category: `images`,
+	accessableby: "Members",
+    aliases: ["real"],
+    perms: [],
+    cmdperms: []
+}
