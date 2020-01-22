0f58cc15c9f08d72e18c8966c1bbd8565b011f53
SpeckyYT <Specky.fans@gmail.com>
2020-01-22 06:55:33 +0100

    Added some Client Options

--- handlers/generalhandler.js
+++ handlers/generalhandler.js
@@ -1,7 +1,12 @@
 const { Client } = require("discord.js");
 
 module.exports = async () => {
-    const bot = new Client({autoReconnect:true});
+    const bot = new Client({
+        autoReconnect : true,
+        messageCacheMaxSize : 2500,
+        fetchAllMembers : true
+    });
+
     require('./missingdirectories')();
     require('./missingfiles')();
     require('./login')(bot);
