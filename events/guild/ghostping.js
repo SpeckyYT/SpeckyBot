7b3d5270a08e0465c2ba27687e71217596d07211
SpeckyYT <Specky.fans@gmail.com>
2020-01-23 19:05:21 +0100

    uhhhh

--- events/guild/ghostping.js
+++ events/guild/ghostping.js
@@ -3,16 +3,17 @@ const { RichEmbed } = require('discord.js')
 module.exports = async (bot, msg) => {
     if(msg.mentions.members.first()){
         msg.mentions.members.forEach(member => {
-            if(msg.author.id != member.user.id)
-            member.send(new RichEmbed()
-                .setTitle('Ghostping')
-                .setThumbnail(msg.guild.iconURL)
-                .setColor('#000000')
-                .setDescription(`You have been Ghostpinged by\n\n**${msg.author.tag}** [${msg.author.id}]\n\nin the server\n\n**${msg.guild}** [${msg.guild.id}]`)
-                .setImage(msg.author.avatarURL)
-                .setFooter(`Date of the ghostping`)
-                .setTimestamp(new Date())
-            )
+            if(msg.author.id != member.user.id){
+                member.send(new RichEmbed()
+                    .setTitle('Ghostping')
+                    .setThumbnail(msg.guild.iconURL)
+                    .setColor('#000000')
+                    .setDescription(`You have been Ghostpinged by\n\n**${msg.author.tag}** [${msg.author.id}]\n\nin the server\n\n**${msg.guild}** [${msg.guild.id}]`)
+                    .setImage(msg.author.avatarURL)
+                    .setFooter(`Date of the ghostping`)
+                    .setTimestamp(new Date())
+                )
+            }
         })
     }
 }
