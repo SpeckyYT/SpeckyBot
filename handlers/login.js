1e6dd03e2dbb673d736002df68cae0f3f48a6413
SpeckyYT <Specky.fans@gmail.com>
2020-01-28 21:19:59 +0100

    no idea if this changes anything ://

--- handlers/login.js
+++ handlers/login.js
@@ -2,17 +2,16 @@ module.exports = async (bot) => {
     const { token, prefix } = bot.config;
         
     bot.login(token)
-    .then(() => {
+    .then(async () => {
+        bot.log(`Bot prefix: ${prefix}`.startupinfo);
         bot.log(`Logged as ${bot.user.tag}!`.startupinfo)
     })
-    .catch(() => {
-        Array(25).forEach(() => {
+    .catch(async () => {
+        await Array(25).forEach(async () => {
             console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`.error)
         })
         if(token == "TOKEN_HERE"){
             process.exit(0)
         }
     })
-
-    bot.log(`Bot prefix: ${prefix}`.startupinfo);
 }
