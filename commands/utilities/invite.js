f621e595971d1cb044f2b616f73543252aad5cff
SpeckyYT <Specky.fans@gmail.com>
2020-01-28 22:05:58 +0100

    Update invite.js

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -1,15 +1,16 @@
 const { RichEmbed } = require('discord.js')
 
 module.exports.run = async (bot, msg) => {
-    let link;
-    await bot.generateInvite(2147483135).then(data => {
-        link = data;
-    })
+    if(!bot.link){
+        await bot.generateInvite(2147483135).then(data => {
+            bot.link = data;
+        }).catch(()=>{})
+    }
     
     const embed = new RichEmbed()
         .setColor("#FF00AA")
 		.setTitle('Links!')
-        .setDescription(`[SpeckyBot Discord Server](https://discord.gg/4EecFku)\n[Bot Invite](${link})\n[Support This Bot](https://patreon.com/SpeckyBot)`)
+        .setDescription(`[SpeckyBot Discord Server](https://discord.gg/4EecFku)\n[Bot Invite](${bot.link})\n[Support This Bot](https://patreon.com/SpeckyBot)`)
 	msg.channel.send(embed);
 }
 
