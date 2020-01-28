bfcf130a4dec15526e0e786744d5bf1c267f5771
SpeckyYT <Specky.fans@gmail.com>
2020-01-28 21:21:00 +0100

    removed useless asyncs

--- handlers/login.js
+++ handlers/login.js
@@ -2,12 +2,12 @@ module.exports = async (bot) => {
     const { token, prefix } = bot.config;
         
     bot.login(token)
-    .then(async () => {
+    .then(() => {
         bot.log(`Bot prefix: ${prefix}`.startupinfo);
         bot.log(`Logged as ${bot.user.tag}!`.startupinfo)
     })
     .catch(async () => {
-        await Array(25).forEach(async () => {
+        await Array(25).forEach(() => {
             console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`.error)
         })
         if(token == "TOKEN_HERE"){
