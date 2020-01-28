79afb0f17550624bb6178527e118e0bfe6c6bcd4
SpeckyYT <Specky.fans@gmail.com>
2020-01-28 21:44:15 +0100

    Update login.js

--- handlers/login.js
+++ handlers/login.js
@@ -7,9 +7,7 @@ module.exports = async (bot) => {
         bot.log(`Logged as ${bot.user.tag}!`.startupinfo)
     })
     .catch(async () => {
-        await Array(25).forEach(() => {
-            console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`.error);
-        })
+        console.log(`PLEASE EDIT THE CONFIG.JSON FILE (token is incorrect or can't login to discord)`.error);
         if(token == "TOKEN_HERE"){
             await bot.wait(10000)
             process.exit(0)
