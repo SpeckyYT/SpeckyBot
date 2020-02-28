d7936d7a6c3c1582e0db268f35a2a5a50f9924c6
SpeckyYT <Specky.fans@gmail.com>
2020-02-28 17:15:15 +0100

    added async primes commands

---
+++ commands/misc/primes.js
@@ -0,0 +1,55 @@
+module.exports = {
+    name: "primes",
+	description: "Gives you prime numbers.",
+    usage: `<base>`,
+    category: `misc`,
+	accessableby: "Members",
+    aliases: ["prime"]
+}
+
+module.exports.run = async (bot, msg) => { 
+    let primes = [];
+    let numb = 1;
+    let string = "";
+
+    let base = msg.Args[0];
+
+    if(!base || isNaN(base)){
+        base = 10;
+    }
+
+    base = Math.min(Math.max(base, 2), 36);
+
+    let promises = [];
+
+    async function prime(params){
+        while(true){
+            numb++;
+
+            let temp = 1;
+            
+            while(temp+1 < numb){
+                temp++;
+
+                if(numb % temp == 0){
+                    temp = false;
+                    break;
+                }
+            }
+
+            if(temp){
+                primes.push(numb.toString(base));
+                console.log(primes.length)
+                string = `\`\`\`${primes.join(" ")}\`\`\``;
+
+                if(string.length >= 2000){
+                    primes.pop();
+                    break;
+                }
+            }
+        }
+    }
+    await prime();
+
+    return msg.channel.send(`\`\`\`${primes.join(" ")}\`\`\``)
+}
