012010f4dfd882558e2d114cbf720799087a92c0
SpeckyYT <Specky.fans@gmail.com>
2020-03-11 17:33:02 +0100

    bot.embed.timestamp isn't used anymore

--- events/client/updatebot.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    event: "interval_1_sec"
-}
-
-module.exports.call = async bot => {
-    try{
-        bot.embed.timestamp = Math.ceil(new Date().getTime() / 1000) * 1000;
-    }catch(err){}
-}
