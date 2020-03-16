5a978811742fac42d34413a51fcc39960e941b30
SpeckyYT <Specky.fans@gmail.com>
2020-03-16 17:12:02 +0100

    minor fix for Crafter

--- events/custom/anti-cs-ads.js
+++ events/custom/anti-cs-ads.js
@@ -11,22 +11,22 @@ module.exports.call = async (bot, oldMember, newMember) => {
     //server limiter
     if(!limited.includes(newMember.guild.id)) return;
 
-    let memberRole = '265519525041143809'
-    let adInStatusRole = '638091940545560639'
+    let removeRoles = ['607522930800459789','269841158694371329','605392884526612524','599988443476525087','309678822435848204','268409019641626626','268409297338105867','265519525041143809']
+    let addRoles = ['638091940545560639']
 
     let regex = /(https?:\/\/)?(www\.)?((discord\.(gg|io|me|li)|discordapp\.com\/invite|invite\.gg))(\/|\\\/|\\)[A-z+0-9]{1,}/g;
 
     newMember.guild.members.forEach(member => {
         member.roles.forEach(role => {
-            if(role.id == memberRole){
+            if(role.id == removeRoles){
                 if(member.user.presence.game){
                     let game = member.user.presence.game;
     
                     [game.name, game.state].forEach(item => {
                         if(item){
                             if(item.toLowerCase().match(regex)){
-                                try{member.addRole(adInStatusRole)}catch(e){}
-                                try{member.removeRole(memberRole)}catch(e){}
+                                try{member.addRole(addRoles)}catch(e){}
+                                try{member.removeRole(removeRoles)}catch(e){}
                             }
                         }
                     })
