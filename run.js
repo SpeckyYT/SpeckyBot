8c896a1cfe462909c9462b609a928b98ab1b821e
SpeckyYT <Specky.fans@gmail.com>
2020-04-26 13:38:08 +0200

    Update run.js

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 const { spawn } = require('child_process');
 
-const CP = spawn(`${__dirname}\\src\\bot.bat`,{stdio: [process.stdin, process.stdout, process.stderr]});
+const CP = spawn(`${__dirname}/src/bot.bat`,{stdio: [process.stdin, process.stdout, process.stderr]});
 
 CP.on('exit', code => {
     console.log(`Bot died with code ${code}`);
