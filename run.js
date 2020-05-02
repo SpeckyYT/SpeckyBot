a62874cef6f4d718c79decf9022460ab1433d71c
SpeckyYT <Specky.fans@gmail.com>
2020-05-02 19:25:00 +0200

    finally removed the main Child_Process

--- run.js
+++ run.js
@@ -1,8 +1,3 @@
-const { spawn } = require('child_process');
-
-const CP = spawn(`${__dirname}/src/bot.bat`,{stdio: [process.stdin, process.stdout, process.stderr]});
-
-CP.on('exit', code => {
-    console.log(`Bot died with code ${code}`);
-    process.exit(code);
-});
+const mainfolder = './src'
+process.chdir(mainfolder);
+require(mainfolder+'/bot.js');
