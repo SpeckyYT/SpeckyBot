7b3891cb96cbe15a041fbb67de94815f30578f2f
SpeckyYT <Specky.fans@gmail.com>
2020-07-06 17:36:56 +0200

    Create bestchatter.js

---
+++ src/commands/misc/bestchatter.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: "bestchatters",
+    description: "Gives the best chatters of the chat!",
+    usage: `[#channel]`,
+    category: `misc`,
+    accessableby: "Members",
+    aliases: ["bestchatter","bc"]
+}
+
+const { Collection } = require('discord.js');
+
+module.exports.run = async (bot, msg) => {
+    const channel = msg.mentions.channels.first() || msg.channel;
+    const peoples = new Collection();
+
+    await channel.fetchMessages({limit: 100})
+    .then(m => m.map(m => m.author))
+    .then(a => a.forEach(a => peoples.set(a.id, peoples.get(a.id) ? [a, peoples.get(a.id)[1] + 1] : [a, 1])))
+
+    return msg.channel.send(
+        bot.embed()
+        .setTitle("Best Chatters!")
+        .setDescription(peoples.sort((a,b) => b[1]-a[1]).map(v => `${v[0]}`+(v[0].bot?' **(bot)**':'')+`: ${v[1]} messages`).join('\n'))
+        .setFooter(`last 100 messages in ${channel.name}`)
+    )
+}
