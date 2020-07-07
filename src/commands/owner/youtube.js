45f169b20b3cbb3bf63881a3c70ac58bdb5b352d
SpeckyYT <Specky.fans@gmail.com>
2020-07-07 22:05:11 +0200

    added youtube video download

---
+++ src/commands/owner/youtube.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "youtubedl",
+    description: "Downloads a video",
+    usage: "<video link>",
+    category: "owner",
+    aliases: ['ydl']
+}
+
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
+
+module.exports.run = (bot, msg) => {
+    const video = youtubedl(msg.content,['--format=18'],{cwd:__dirname});
+    video.on('info', info => {
+        video.pipe(fs.createWriteStream("../"+"YT-"+info._filename));
+    })
+}
