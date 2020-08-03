a9e1de854afd6c6d6c4003f0b4bf6562d2cd8c54
SpeckyYT <Specky.fans@gmail.com>
2020-08-03 22:47:57 +0200

    fixed helloworlds

--- src/commands/helloworld/HelloWorld.template.ts
+++ src/commands/helloworld/HelloWorld.template.ts
@@ -2,12 +2,11 @@ import { SpeckyClient } from "../../../typings/Client";
 import { CmdMessage } from "../../../typings/Message";
 
 module.exports = {
-    name: "hwts",
+    name: "hwtts",
     description: "Hello World!",
     usage: '',
     category: 'helloworld',
     aliases: [],
-    run: async (bot:SpeckyClient, msg:CmdMessage): Promise<void> => {
-        await msg.channel.send("`TypeScript`: Hello World!");
-    }
+    type: 'template',
+    run: async (bot:SpeckyClient, msg:CmdMessage): Promise<string> => "`TypeScript (templates)`: Hello World!"
 }
