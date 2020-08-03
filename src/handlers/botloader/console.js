e7cedaec474ba66df2bd0a079fd2216634f73f44
SpeckyYT <Specky.fans@gmail.com>
2020-08-03 15:31:48 +0200

    done recursive folders for console

--- src/handlers/botloader/console.js
+++ src/handlers/botloader/console.js
@@ -1,4 +1,5 @@
-const { readdirSync } = require('fs');
+const { readdirSync, lstatSync } = require('fs');
+const { join } = require('path');
 const { Collection } = require('discord.js');
 const promisify = require('promisify-func');
 
@@ -6,19 +7,42 @@ module.exports = async (bot) => {
     bot.console = new Collection();
     bot.consoleali = new Collection();
 
-    readdirSync(`./console/`)
-    .filter(d => d.match(bot.supportedFiles))
-    .forEach(async file => {
-        try{
-            const pull = bot.require(`./console/${file}`);
-            bot.console.set(pull.name, pull);
-            if (pull.aliases) pull.aliases.forEach(a => bot.consoleali.set(a, pull.name));
-            bot.log(`${file}`.debug);
-        }catch(err){
-            bot.log(`${file} ERROR!`.error);
-            bot.log(err.message.error);
+    const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(source => lstatSync(source).isDirectory());
+    const getFiles = source => readdirSync(source).map(name => join(source, name)).filter(source => !lstatSync(source).isDirectory()).map(f => f.slice(f.lastIndexOf('\\')+1));
+
+    function loadFolders(path = []){
+        const currPath = path.join('/');
+        const stringPath = currPath.slice(currPath.indexOf('\\')+1);
+
+        const files = getFiles(`./${currPath}/`);
+        if(!files.includes('.ignorefiles')){
+            files.filter(d => d.match(bot.supportedFiles))
+            .forEach(file => {
+                try{
+                    const pull = bot.require(`./console/${file}`);
+                    bot.console.set(pull.name, pull);
+                    if (pull.aliases) pull.aliases.forEach(a => bot.consoleali.set(a, pull.name));
+                    bot.log(`${stringPath.padEnd(32,' ')}|${' '.repeat(8)}${file}`.debug);
+                }catch(err){
+                    bot.log(`${stringPath.padEnd(32,' ')}|${' '.repeat(8)}${file} ERROR!`.error);
+                    bot.log(err.message.error);
+                }
+            })
         }
-    })
+
+        if(!files.includes('.ignoredirs')){
+            getDirectories(`./${currPath}/`)
+            .forEach(dir => {
+                try{
+                    loadFolders([dir]);
+                }catch(err){
+                    bot.log(`ERROR WHILE LOADING ${stringPath+"/"+dir} FOLDER!`.error);
+                    bot.log(String(err).error);
+                }
+            })
+        }
+    }
+    loadFolders(['console']);
 
     process.openStdin().removeAllListeners();
 
