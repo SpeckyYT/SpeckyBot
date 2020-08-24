2d062b5d49edd20122250f825d60f3c203b7b8ab
SpeckyYT <Specky.fans@gmail.com>
2020-08-24 14:52:14 +0200

    added truth or dare

---
+++ src/commands/games/dare.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "dare",
+    description: "Gives you a task which you accomplish!",
+    usage: "",
+    category: "games",
+    aliases: []
+}
+
+// Tasks from:
+// https://improb.com/best-truth-or-dare-questions/
+
+module.exports.run = async (bot, msg) => {
+    const questions = (require('.\\data\\tod-d') || '').split('\n');
+    const question = questions.pick();
+    return msg.channel.send(
+        bot.embed()
+        .setTitle("Dare!")
+        .setDescription(question || "Like SpeckyBot!")
+    );
+}
---
+++ src/commands/games/truth.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "truth",
+    description: "Gives you a question which you should answer honestly!",
+    usage: "",
+    category: "games",
+    aliases: []
+}
+
+// Questions from:
+// https://hobbylark.com/party-games/truth-or-dare-questions
+// https://improb.com/best-truth-or-dare-questions/
+
+module.exports.run = async (bot, msg) => {
+    const questions = (require('.\\data\\tod-t') || '').split('\n');
+    const question = questions.pick();
+    return msg.channel.send(
+        bot.embed()
+        .setTitle("Truth!")
+        .setDescription(question || "Do you like SpeckyBot?")
+    );
+}
