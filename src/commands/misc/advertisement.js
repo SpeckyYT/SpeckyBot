4f8a3782b01fb2221cec4d1026eed4ca27766b79
SpeckyYT <Specky.fans@gmail.com>
2020-08-27 20:07:20 +0200

    created better code for advertisement

--- src/commands/misc/advertisement.js
+++ src/commands/misc/advertisement.js
@@ -3,29 +3,34 @@ module.exports = {
     description: "Gives you a SpeckyBot Advertisement!",
     usage: "",
     category: `misc`,
-    aliases: ["advertisements"]
+    aliases: ["advertisements","adv"]
 }
 
 module.exports.run = (bot, msg) => {
-    return msg.channel.send(
-        bot.embed()
-        .setTitle("Why Should People Use SpeckyBot?")
-        .setDescription("TL;DR SpeckyBot is great for simple people searching for great times.")
-        .addField(
-            "1) SpeckyBot has more than 150 commands available for everyone",
-            "those are enough for everyone!"    
-        )
-        .addField(
-            "2) SpeckyBot has a lot of cool features",
-            "for example `global channel` `channel topic filters` and other more!"
-        )
-        .addField(
-            "3) SpeckyBot is Open-Source and is regularly mantained",
-            "because a good bot requires good maintenance!"
-        )
-        .addField(
-            "4) SpeckyBot has a foolproof way for writing any kind of command",
-            "perfect for any newcomer to coding in JavaScript!"
-        )
-    )
+    const emb = bot.embed()
+    .setTitle("Why Should People Use SpeckyBot?")
+    .setURL(bot.link)
+    .setDescription("TL;DR SpeckyBot is great for simple people searching for great times.");
+
+    [
+        [
+            "SpeckyBot has more than 150 commands available for anyone",
+            "those are enough for everyone!"
+        ],[
+            "SpeckyBot has a lot of cool features",
+            "like `global channel` `channel topic filters` and more!"
+        ],[
+            "SpeckyBot has games which are fun to play",
+            "like `speckysays`, `truth or dare`, `bw` and more!"
+        ],[
+            "SpeckyBot is Open-Source and is regularly mantained",
+            "because a good bot requires a lot of maintenance!"
+        ],[
+            "SpeckyBot has a foolproof way for writing any kind of command",
+            "perfect for any newcomer to coding in JavaScript, CoffeeScript or TypeScript!"
+        ]
+    ]
+    .map((adv, i) => emb.addField(`${i+1}) ${adv[0]}`, adv[1]))
+
+    return msg.channel.send(emb);
 }
