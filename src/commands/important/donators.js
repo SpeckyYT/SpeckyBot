753855f4c63a93d70640c3c8712f39e1dde372c8
SpeckyYT <Specky.fans@gmail.com>
2020-08-28 22:41:31 +0200

    created messagelinks and updated donators

--- src/commands/important/donators.js
+++ src/commands/important/donators.js
@@ -7,9 +7,10 @@ module.exports = {
 
 module.exports.run = async (bot, msg) => {
     const don = {
-        Benjiman: [1.15,'€']
+        Benjiman: [1.15,'€'], // 525006281703161867
+        "Dav!d" : [1.16,'€']  // 555791735607787580
     };
-    const donators = don.toCollection().sort((a, b) => a[0] - b[0]);
+    const donators = don.toCollection().sort((a, b) => b[0] - a[0]);
 
     const embed = bot.embed()
     .setTitle("Donators!")
@@ -17,7 +18,7 @@ module.exports.run = async (bot, msg) => {
     .setDescription("Here you'll find the donators of SpeckyBot!")
     .setFooter("Thank you all for the support!", bot.user.avatarURL)
 
-    donators.forEach((donation, donartor) => embed.addField(donartor, donation[0] + (donation[1]||'€')))
+    donators.forEach((donation, donartor) => embed.addField(donartor, donation[0].toFixed(2) + (donation[1]||'€')))
 
     return msg.channel.send(embed);
 }
---
+++ src/events/messages/messagelinks.js
@@ -0,0 +1,48 @@
+module.exports = {
+    event: "message"
+}
+
+const { RichEmbed } = require('discord.js');
+const poop = "https://images-ext-1.discordapp.net/external/qDr7Y7SwTvJ3D_jZOYRNU-Vak5cGKw3zlZfFT2t-Ihc/https/i.imgur.com/jNz2Dwp.png"
+
+module.exports.call = async (bot, msg) => {
+    if (msg.author.bot) return;
+    await bot.loadSettings();
+    const u_settings = bot.settings.user || {};
+    if(!(u_settings[msg.author.id] ? u_settings[msg.author.id].messagelink : false)) return;
+
+    const m = msg.extend();
+    if(!m.links.length) return;
+
+    const perms = msg.guild.me.permissionsIn(msg.channel).toArray();
+    if(!perms.includes('SEND_MESSAGES')) return;
+
+    const regex = /https?:\/\/(?:\w+\.)?discord(?:app)?\.com\/channels\/(\d+)\/(\d+)\/(\d+)\/?/g;
+
+    m.links.unique().forEach(async link => {
+        if(m.content.includes(`<${link}>`)) return;
+
+        const discordLink = regex.exec(link);
+        if(!discordLink) return;
+        const [, serverID, channelID, messageID] = discordLink;
+        const server = bot.guilds.get(serverID);
+        const channel = bot.channels.get(channelID);
+        const message = await (channel && channel.fetchMessage && channel.fetchMessage(messageID));
+
+        try{
+            await msg.channel.send(
+                new RichEmbed()
+                .setAuthor(message ? `${message.author.tag} (ID: ${message.author.id})` : "Unknown User", message ? message.author.avatarURL : poop)
+                .setDescription(`[Message](${link}) in <#${channelID}>\n${message ? message.content || "" : "Unknown Message"}`)
+                .setFooter(`${server ? server.name : "Unknown Server"} - Quoted by ${msg.author.tag}`, server ? server.iconURL : poop)
+                .setTimestamp(message && message.createdAt || messageID.snowflake && messageID.snowflake().date)
+                .setColor(message ? 'GREEN' : 'RED')
+            )
+            if(message && message.embeds && message.embeds.length){
+                message.embeds.forEach(embed =>
+                    msg.channel.send(new RichEmbed(embed)).catch(()=>{})
+                )
+            }
+        }catch(e){}
+    })
+}
