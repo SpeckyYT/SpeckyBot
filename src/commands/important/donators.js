20d2e4f8062fec729e8e763eed2cc1a4ca50618c
SpeckyYT <Specky.fans@gmail.com>
2020-08-31 11:52:20 +0200

    Update donators.js

--- src/commands/important/donators.js
+++ src/commands/important/donators.js
@@ -12,13 +12,13 @@ module.exports.run = async (bot, msg) => {
     };
     const donators = don.toCollection().sort((a, b) => b[0] - a[0]);
 
+    const string = donators.map((donation, donator) => `**${donator}**: ${donation[0].toFixed(2)}${donation[1]||'€'}`).join('\n');
+
     const embed = bot.embed()
     .setTitle("Donators!")
     .setURL("https://www.paypal.me/speckyy")
-    .setDescription("Here you'll find the donators of SpeckyBot!")
-    .setFooter("Thank you all for the support!", bot.user.avatarURL)
-
-    donators.forEach((donation, donartor) => embed.addField(donartor, donation[0].toFixed(2) + (donation[1]||'€')))
+    .setDescription(`Here you'll find the donators of SpeckyBot!\n\n${string}`)
+    .setFooter("Thank you all for the support!", bot.user.avatarURL);
 
     return msg.channel.send(embed);
 }
