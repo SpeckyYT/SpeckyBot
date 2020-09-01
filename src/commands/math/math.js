b3a2fe5706d836e568827a1064234db5f413fcda
SpeckyYT <Specky.fans@gmail.com>
2020-09-01 22:12:23 +0200

    Update math.js

--- src/commands/math/math.js
+++ src/commands/math/math.js
@@ -9,19 +9,19 @@ module.exports = {
 const { evaluate } = require("mathjs");
 
 module.exports.run = async (bot, msg) => {
-    const { content } = msg;
-    if(!content) return msg.channel.send('Please input a calculation.');
+    const { cmdContent } = msg;
+    if(!cmdContent) return bot.cmdError('Input a calculation');
 
     let resp;
     try{
-        resp = await evaluate(content);
+        resp = await evaluate(cmdContent);
     } catch (e){
-        return msg.channel.send('Sorry, please input a valid calculation.')
+        return bot.cmdError('Input a valid calculation')
     }
     const embed = bot.embed()
     .setColor("#FFFFFF")
     .setTitle('Math Calculation')
-    .addField("Input", `\`\`\`js\n${content}\`\`\``)
+    .addField("Input", `\`\`\`js\n${cmdContent}\`\`\``)
     .addField("Output", `\`\`\`js\n${resp}\`\`\``)
-    msg.channel.send(embed);
+    return msg.channel.send(embed);
 }
