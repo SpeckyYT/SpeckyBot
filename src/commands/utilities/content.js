c6f2ac44d5ff1000da7a2872dff9a1bc050dd53d
SpeckyYT <Specky.fans@gmail.com>
2020-09-11 16:43:25 +0200

    fixed flags in message content

--- src/commands/utilities/content.js
+++ src/commands/utilities/content.js
@@ -5,21 +5,20 @@ module.exports = {
     usage: "<text>"
 }
 
-import { SpeckyClient } from "../../../typings/Client";
-import { CmdMessage } from "../../../typings/Message";
-
-module.exports.run = (bot:SpeckyClient,msg:CmdMessage) => {
+module.exports.run = (bot,msg) => {
     const {content, args, Args, ARGS, cmdContent, command, links, _flags} = msg;
-    return msg.channel.send(`\`\`\`js\n${
+    return msg.channel.send(
+        "```js\n"+
         JSON.stringify({
+            command,
             content,
             cmdContent,
             args,
             Args,
             ARGS,
-            command,
             links,
             _flags
-        },null,4)
-    }\n\`\`\``);
+        },null,2)
+        +"\n```"
+    );
 }
--- src/prototypes/message.js
+++ src/prototypes/message.js
@@ -10,8 +10,15 @@ module.exports = bot => {
         /** The Cloned Message Object. */
         const msg = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
 
+        // Flags
+        const flags = msg.content.toLowerCase().match(/^--(\w+)$/);
+        msg._flags = [];
+        if(flags) msg._flags = flags.map(f=>f.slice(2)); // removes the "--" on the beginning
+        msg.hasFlag = (input) => msg._flags.includes(input.toLowerCase());
+        msg.flag = msg.hasFlag;
+
         // Args
-        msg.Args = msg.content.split(/\s|\n/g);
+        msg.Args = msg.content.split(/\s|\n/g).filter(a => !a.match(/^--(\w+)$/));
         while(msg.Args[0] == bot.config.prefix && msg.Args.length > 0){
             const fix = msg.Args[0] + msg.Args[1];
             msg.Args[1] = fix;
@@ -24,14 +31,6 @@ module.exports = bot => {
 
         // Links
         msg.links = (msg.content ? msg.content.match(/(https?:\/\/(www\.)?)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/g) : []) || []
-
-
-        // Flags
-        const flags = msg.content.toLowerCase().match(/--(\w+)/g);
-        msg._flags = [];
-        if(flags) msg._flags = flags.map(f=>f.slice(2)); // removes the "--" on the beginning
-        msg.hasFlag = (input) => msg._flags.includes(input.toLowerCase());
-        msg.flag = msg.hasFlag;
 
 
         // Client
