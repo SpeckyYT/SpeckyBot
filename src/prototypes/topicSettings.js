8b8400b6031e99f5b596cc16c556a793959a2d2e
SpeckyYT <Specky.fans@gmail.com>
2020-09-13 17:00:14 +0200

    Update topicSettings.js

--- src/prototypes/topicSettings.js
+++ src/prototypes/topicSettings.js
@@ -3,11 +3,11 @@ const { Channel } = require('discord.js');
 module.exports = () => {
 
     Channel.prototype.topicSetting = function(string=''){
-        return this.topic ? this.topic.toLowerCase().includes(`[${string}]`) : false;
+        return this.topic ? this.topic.toLowerCase().includes(`[${string.toLowerCase()}]`) : false;
     }
 
     String.prototype.topicSetting = function(string=''){
-        return this.toLowerCase().includes(`[${string}]`);
+        return this.toLowerCase().includes(`[${string.toLowerCase()}]`);
     }
 
 }
