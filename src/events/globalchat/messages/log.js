9b51a1b2e6d72294372d134583b8edd5e87510bd
SpeckyYT <Specky.fans@gmail.com>
2020-09-17 14:25:57 +0200

    added bannedUsers global chat protection

--- src/events/globalchat/messages/log.js
+++ src/events/globalchat/messages/log.js
@@ -8,12 +8,12 @@ module.exports.call = async (bot, msg) => {
     const file = __dirname+'\\log\\log.json';
     fs.readFile(file, {encoding:"utf-8"}, (err,data) => {
         if(err){
-            return fs.writeFile(file,JSON.stringify([msg.content||'']),{encoding:"utf-8"},()=>{});
+            return fs.writeFile(file,JSON.stringify([msg.content||''],null,0),{encoding:"utf-8"},()=>{});
         }
         const json = JSON.parse(data);
         if(msg.content){
             json.push(msg.content);
-            fs.writeFile(file, JSON.stringify(json), {encoding:"utf-8"}, ()=>{});
+            fs.writeFile(file, JSON.stringify(json,null,2), {encoding:"utf-8"}, ()=>{});
         }
     })
 }
