e37896a2bf92ee7538796a7ea9213b2dd30d1d99
SpeckyYT <Specky.fans@gmail.com>
2020-09-18 10:15:10 +0200

    fixed music error on first launch

--- src/handlers/botloader/music.js
+++ src/handlers/botloader/music.js
@@ -1,5 +1,6 @@
 const musicClient = require('la-music-core');
 
 module.exports = async (bot) => {
+    if(!(bot.config && bot.config.youtube)) return;
     bot.music = new musicClient(bot.config.youtube);
 };
