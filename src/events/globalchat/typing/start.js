cfe9c2980900474c67be22e963ea9f614d1c71c0
SpeckyYT <Specky.fans@gmail.com>
2020-09-20 19:40:05 +0200

    fixed typing in own server

--- src/events/globalchat/typing/start.js
+++ src/events/globalchat/typing/start.js
@@ -5,6 +5,6 @@ module.exports = {
 module.exports.call = (bot,channel,user) => {
     if(user.bot) return;
     if(bot.globalchats.has(channel.id)){
-        bot.globalchats.forEach(c => c.startTyping(1));
+        bot.globalchats.filter(c => c.id != channel.id).forEach(c => c.startTyping(1));
     }
 }
--- src/events/globalchat/typing/stop.js
+++ src/events/globalchat/typing/stop.js
@@ -5,6 +5,6 @@ module.exports = {
 module.exports.call = (bot,channel,user) => {
     if(user.bot) return;
     if(bot.globalchats.has(channel.id)){
-        bot.globalchats.forEach(c => c.stopTyping());
+        bot.globalchats.filter(c => c.id != channel.id).forEach(c => c.stopTyping());
     }
 }
