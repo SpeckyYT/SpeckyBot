0763a465779c31e176627f5d36bdbc3dcf34c1cd
SpeckyYT <Specky.fans@gmail.com>
2020-09-26 11:21:50 +0200

    fixed 3musk system

--- src/events/custom/ChillAndPlay/3musk/3musk remove.js
+++ src/events/custom/ChillAndPlay/3musk/3musk remove.js
@@ -3,8 +3,28 @@ module.exports = {
 }
 
 // server limiter
-// const limited = ["265505748413448193"]
+const limited = ["265505748413448193"]
 
 module.exports.call = async (bot, oldPresence, newPresence) => {
-    return; // Disabled
+    const member = newPresence.member;
+
+    // server limiter
+    if(!limited.includes(member.guild.id)) return;
+
+    const status = member.user.presence.status;
+
+    if(!["offline","idle"].includes(status)) return;
+
+    const muskRole = "636272631984947240"
+    const muskGateRole = "663303390620680193"
+
+    if(member.roles.has(muskRole)){
+        try{
+            member.removeRole(muskRole)
+        }catch{}
+
+        try{
+            member.addRole(muskGateRole)
+        }catch{}
+    }
 }
