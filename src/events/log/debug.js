ee4c4a37188028e0182f239908b959f39011dda7
SpeckyYT <Specky.fans@gmail.com>
2020-09-26 12:03:28 +0200

    less logging

--- src/events/log/debug.js
+++ src/events/log/debug.js
@@ -7,7 +7,8 @@ module.exports.call = async (bot, data) => {
     if(!channel) return;
     if(data.includes("WS")) return;
     if(data.includes("VOICE")) return;
-    if(data.match(/\d{1,3}\.\d{1,3}\.\d{1,3}/g)) return
+    if(data.match(/\d{1,3}\.\d{1,3}\.\d{1,3}/g)) return;
+    if(data.includes("hit on route")) return;
     if(data.includes(bot.token)) return;
     return channel.send(data);
 }
