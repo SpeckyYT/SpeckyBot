f1d08f367f0cba365890e939e58f0feeb5c18f1d
SpeckyYT <Specky.fans@gmail.com>
2020-10-01 09:26:33 +0200

    Update offline.js

--- src/events/custom/SpeckysBots/offline.js
+++ src/events/custom/SpeckysBots/offline.js
@@ -1,14 +1,15 @@
 module.exports = {
-    event: ["interval_1_min","guildMemberUpdate"]
+    event: ["interval_1_min","presenceUpdate"]
 }
 
 const speckysBots = "538028973058424832";
 const offlineRole = "760554589040279592";
 
-module.exports.call = async (bot, oldMember, newMember) => {
+module.exports.call = async (bot, oldPresence, newPresence) => {
     if(newMember.user.bot) return;
     const guild = bot.guilds.cache.get(speckysBots);
     if(!guild) return;
+    /*
     if(oldMember && newMember){
         if(newMember.guild.id != speckysBots) return;
         const isOffline = newMember.presence.status == 'offline';
@@ -22,7 +23,7 @@ module.exports.call = async (bot, oldMember, newMember) => {
             .catch(()=>{});
         }
     }
-
+    */
     guild.members.cache
     .filter(m => !m.user.bot)
     .filter(m => (m.presence.status == 'offline') ^ (m.roles.cache.has(offlineRole)))
