854760423738893d715a600a7d93538b8592f8cc
SpeckyYT <Specky.fans@gmail.com>
2020-10-07 19:59:48 +0200

    Update gitupdate.js

--- src/commands/owner/gitupdate.js
+++ src/commands/owner/gitupdate.js
@@ -7,8 +7,20 @@ module.exports = {
 const cp = require('child_process');
 
 module.exports.run = async (bot, msg) => {
-    cp.exec('git pull origin',()=>{})
-    .on('close', () => {
-        return msg.channel.send('Bot should be successfully updated!');
-    });
+    function run(command){
+        return new Promise((res,rej) => {
+            cp.exec('',()=>{})
+            .on('error', () => rej())
+            .on('close', () => res());
+        })
+    }
+
+    try{
+        await run('git reset --hard origin/master');
+        await run('git fetch --all');
+        await run('git pull origin');
+        return bot.cmdSuccess('Bot should be successfully updated!');
+    }catch(err){
+        return bot.cmdError("Wasn't able to update the bot");
+    }
 }
