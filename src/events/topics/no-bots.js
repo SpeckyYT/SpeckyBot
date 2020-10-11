1beb1234176329c4060087a039a01b0481aacf2e
SpeckyYT <Specky.fans@gmail.com>
2020-10-11 14:45:18 +0200

    fixed useless .topicSetting checks

--- src/events/topics/no-bots.js
+++ src/events/topics/no-bots.js
@@ -4,7 +4,7 @@ module.exports = {
 
 module.exports.call = async (bot, msg) => {
     if(msg.channel.type == 'dm') return;
-    if(msg.channel.topicSetting ? msg.channel.topicSetting('no-bots') : false){
+    if(msg.channel.topicSetting('no-bots')){
         if(msg.author.bot) msg.delete().catch(()=>{})
     }
 }
