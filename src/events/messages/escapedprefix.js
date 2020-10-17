8e95ed4c762cee9ff6b8a45bf74881b1ec3b7763
SpeckyYT <Specky.fans@gmail.com>
2020-10-17 17:51:46 +0200

    fixed escaped prefix for uppercased characters

--- src/events/messages/escapedprefix.js
+++ src/events/messages/escapedprefix.js
@@ -4,11 +4,11 @@ module.exports = {
 
 module.exports.call = async (bot, msg) => {
     if(bot.config.prefix.match(/^[a-zA-Z0-9]*$/g)) return;
-    if(!msg.content.startsWith(bot.config.prefix)){
-        const length = bot.config.prefix.length + [...bot.config.prefix].filter(v => v.match(/\[^0-9a-zA-Z]/g)).length;
+    if(!msg.content.toLowerCase().startsWith(bot.config.prefix)){
+        const length = bot.config.prefix.length + [...bot.config.prefix.toLowerCase()].filter(v => v.match(/\[^0-9a-zA-Z]/g)).length;
         const regex = /(?<![^\\]\\)\\(?=[^\da-zA-Z\\])|(?<!\\)\\(?=\\)/g;
-        const content = msg.content.slice(0,length+1);
-        if(content.replace(regex,'') == bot.config.prefix){
+        const content = msg.content.slice(0,length+1).toLowerCase();
+        if(content.replace(regex,'') == bot.config.prefix.toLowerCase()){
             const emoji = ['clownglasses','fratm','stress1','stress2'].pick();
             const id = bot.emotes[emoji].id();
             if(!id) return;
