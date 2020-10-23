a684ee51836cf7a4c0cac346fafd737698668015
SpeckyYT <Specky.fans@gmail.com>
2020-10-23 20:53:11 +0200

    made a decent reload system

--- src/botfunctions/reload.js
+++ src/botfunctions/reload.js
@@ -4,7 +4,44 @@ module.exports = (bot) => {
     bot.reload = () => {
         const begin = new Date();
 
-        require(join(process.cwd(),'generalhandler'))(bot);
+        function load(handlers){
+            for(let handler of handlers){
+
+                const log = bot.log || console.log;
+
+                let loading = `\n\nLoading ${handler.toUpperCase()}!\n`;
+
+                if('_'.dependency){
+                    loading = loading.dependency;
+                }
+
+                log(loading);
+
+                try{
+                    bot.require(join(process.cwd(),'handlers',handler))(bot);
+                }catch(err){
+                    log(`handler\t${handler}`.toUpperCase().error);
+                    log("FATAL ERROR ON HANDLERS".fatal);
+                    log(err);
+                    process.exit(1);
+                }
+            }
+        }
+
+        const reloadable = [
+            "botfunctions",
+            "commands",
+            "console",
+            "dependencies",
+            "emotes",
+            "events",
+            "languages",
+            "modules",
+            "prototypes",
+            "templates"
+        ];
+
+        load(reloadable);
 
         const end = new Date();
         const time = end - begin;
