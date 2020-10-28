e59a4c5e0181861709c04b2282306de8b0a948fd
SpeckyYT <Specky.fans@gmail.com>
2020-10-28 17:05:34 +0100

    removed .bf languages

--- src/languages/brainfuck.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-module.exports = () => {
-    require.extensions['.b'] = (module, filename) => {
-        module.exports = fs.readFileSync(filename,{encoding: 'utf8'}).split('').filter((v) => '[]<>-+,.'.includes(v));
-    }
-    require.extensions['.bf'] = require.extensions['.b'];
-}
