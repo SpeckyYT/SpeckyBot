bcaf5c682ed65aceb0351ed303492124e8a5416b
SpeckyYT <Specky.fans@gmail.com>
2020-11-02 21:02:51 +0100

    fixed music

--- src/handlers/music.js
+++ src/handlers/music.js
@@ -1,11 +1,10 @@
 const { Player } = require('discord-music-player');
 
 module.exports = async (bot) => {
-    if(!(bot.config && bot.config.youtube)) return;
     bot.music = new Player(
         bot,
-        bot.config.youtube,
         {
+            token: bot.config.youtube,
             leaveOnEmpty: true,
             leaveOnEnd: true
         }
