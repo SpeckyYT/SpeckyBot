822bef198f4a0f37ca062d8cdf5644f9376368a6
SpeckyYT <Specky.fans@gmail.com>
2020-11-30 21:22:05 +0100

    Update sendbuffer.js

--- src/templates/sendbuffer.js
+++ src/templates/sendbuffer.js
@@ -1,9 +1,9 @@
 const error = 'Image not loaded correctly';
 
-module.exports.sendbuffer = ({image,name}) =>
+module.exports.sendbuffer = ({image,data,buffer,file,name}) =>
     (bot,msg) => {
-        if(!image) throw error;
-        const img = typeof image == 'function' ? image() : image;
-        if(!(img instanceof Buffer)) throw error;
-        return msg.channel.send(img.toAttachment(name));
+        const toCheck = image || data || buffer || file;
+        const toSend = typeof toCheck == 'function' ? toCheck() : toCheck;
+        if(!(toSend instanceof Buffer)) throw error;
+        return msg.channel.send(toSend.toAttachment(name||'file'));
     }
