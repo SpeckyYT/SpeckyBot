42e172ce5a3f0ced46fc3d239afebe93a7efabfd
SpeckyYT <Specky.fans@gmail.com>
2020-11-30 21:30:43 +0100

    added orra alias to cüm

--- src/commands/nsfw/cumsluts.js
+++ src/commands/nsfw/cumsluts.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "cumsluts",
     description: "Gives you a cumslut!",
     category: "nsfw",
-    aliases: ["cumslut","cum","cumshot","cumshots","cs"],
+    aliases: ["cumslut","cum","cumshot","cumshots","cs",'orra'],
     template: 'nsfw',
     data: {
         methods: ["cumsluts","cumArts"]
