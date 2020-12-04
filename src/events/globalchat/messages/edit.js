df9afb48d48e6000f05db497f1bb9e3ea444f39c
SpeckyYT <Specky.fans@gmail.com>
2020-12-04 12:49:16 +0100

    anti invites in global chat

--- src/events/globalchat/messages/edit.js
+++ src/events/globalchat/messages/edit.js
@@ -4,6 +4,8 @@ module.exports = {
 
 module.exports.call = async (bot, _, msg) => {
     const am = bot.cache.globalchat.get(msg.id);
+    const inviteRegex = /(https?:\/\/)?(www\.)?((discord\.(gg|io|me|li)|discordapp\.com\/invite|invite\.gg))(\/|\\\/|\\)[A-z+0-9]{1,}/g;
+    msg.content = msg.content.replace(inviteRegex,'https://discord.gg/4EecFku');
     if(am){
         am.forEach(ms => ms.edit(bot.globalChatEmbed(msg)).catch(()=>{}))
     }
