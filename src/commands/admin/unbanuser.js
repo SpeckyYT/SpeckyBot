b4d05924d65e721db3807cda700989cd06559634
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2020-12-15 15:13:04 +0000

    Create unbanuser.js

---
+++ src/commands/admin/unbanuser.js
@@ -0,0 +1,21 @@
+const db = require('quick-db');
+
+module.exports = {
+    name: 'banuser',
+    description: 'Bans a user from using all bot commands',
+    usage: '<@User/ID>',
+    category: 'owner',
+    userPerms: ['ADMINISTRATOR'],
+    botPerms: ['MANAGE_MESSAGES'],
+}
+
+module.exports.run = async (bot, msg) => {
+    const {args} = msg;
+    if (args.length < 1) return msg.channel.send({embed:{title:'Error! 🛑',description:'No one specified.\nUsage: `banuser <@User/ID>`',color:'RED'}});
+    const target = msg.mentions.users.first() || bot.users.cache.get(args[0]);
+    if (!target) return msg.channel.send({embed:{title:'Error! 🛑',description:'Invalid User Mention/ID.\nUsage: `banuser <@User/ID>`',color:'RED'}});
+    let bannedUsers = db.get('bannedUsers');
+    if (bannedUsers.includes(target.id)) return msg.channel.send({embed:{title:'Error! 🛑',description:`User <@${target.id}> is already banned.\nUsage: \`banuser <@User/ID>\``,color:'RED'}});
+    bannedUsers.push('bannedUsers', target.id);
+    msg.channel.send({embed:{title:'Success!',description:`<@${target.id}> is banned from using SpeckyBot!`,color:'GREEN'}});
+}
