0dc972100191458084e52928f1d72cd5ade54be8
SpeckyYT <Specky.fans@gmail.com>
2020-12-18 12:46:37 +0100

    added error on categories for missing "permissions"

--- src/templates/categoryhelp.js
+++ src/templates/categoryhelp.js
@@ -1,13 +1,18 @@
 module.exports.categoryhelp = ({category}) => {
     return function(bot,msg){
-        bot.checkCategory(category,msg)
+        if(!bot.checkCategory(category,msg)){
+            return bot.cmdError("You don't have the permissions to look at the commands in this category")
+        }
         return msg.channel.send(
             bot.embed()
             .setTitle('Help')
             .setDescription(
                 `The bot prefix is: **${bot.config.prefix}**\n\n`+
                 `❯ **${category.highFirst()}**\n`+
-                bot.commands.filter(c => c.category == category).map(c => `\`${c.name}\`\n`).join(' ')
+                bot.commands
+                .filter(c => c.category == category)
+                .map(c => `\`${c.name}\`\n`)
+                .join(' ')
             )
         );
     }
