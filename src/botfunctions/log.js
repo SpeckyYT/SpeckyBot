3ec83898018441f0edbe0cf38dbb258a44c8e159
SpeckyYT <Specky.fans@gmail.com>
2020-12-19 18:58:08 +0100

    fixed bot.log

--- src/botfunctions/log.js
+++ src/botfunctions/log.js
@@ -1,35 +1,18 @@
-const { appendFile, readFile, writeFile } = require('fs');
+const { promises: { appendFile }} = require('fs');
 const { join } = require('path');
 
 module.exports = (bot) => {
-    bot.log = async (content) => {
-        if(content){
-            console.log(String(content).error)
-        }else{
-            console.log()
-        }
-
-        return new Promise((res,rej) => {
+    bot.log = (cont) =>
+        new Promise((res,rej) => {
+            const content = cont ? String(cont) : '';
+            console.log(content.error)
             const log = join(process.cwd(),'..','commands.log');
 
-            appendFile(log,`${content ?
-                String(content)
-                .replace(/[\x1b][[][0-9]{2}m/g,'')
-                .replace(/\t/g,' ').replace(/ +/g,' ')
-                : ''}\n`, err => err && rej(err));
-
-            let file;
-
-            readFile(log, (err,data)=>{
-                if(err) rej(err);
-
-                file = data.toString().split('\n');
-
-                if(file.length > 100000){
-                    while(file.length > 99000) file.shift();
-                    writeFile(log,file.join("\n"), err => err ? rej(err) : res());
-                }
-            })
+            appendFile(log,`${content
+            .replace(/[\x1b][[][0-9]{2}m/g,'')
+            .replace(/\t/g,' ').replace(/ +/g,' ')
+            }\n`)
+            .then(()=>res())
+            .catch((err)=>rej(err))
         })
-    }
 }
