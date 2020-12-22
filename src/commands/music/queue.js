d028ba8de8f34d7819315a4ab325347517a45fa9
SpeckyYT <Specky.fans@gmail.com>
2020-12-22 18:57:24 +0100

    updated queue command

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -9,18 +9,15 @@ module.exports.run = async (bot, msg) => {
     if(!bot.music.isPlaying(msg.guild.id)) throw new Error('Not playing');
     const queue = await bot.music.getQueue(msg.guild.id);
     if(!queue) return bot.cmdError('No queue found.');
+
+    const [current, ...next] = queue.songs;
+
+    const string1 = `Now Playing: ${current.author} - ${current.name}\n\n`.code('css');
+    const string2 = `${next.slice(0,10).map((song,i) => `#${i+1} | ${song.author} - ${song.name}`).join('\n')}`.code('md');
+
     return msg.channel.send(
         bot.embed()
         .setTitle("Queue")
-        .setDescription(
-            queue.songs
-            .slice(0,10)
-            .map((song, i) =>
-                `${i ? `#${i+1}` : `Current:`} - ${song.name} | ${song.author.name} (${song.duration})`
-            )
-            .join('\n')
-            +
-            (queue.songs.length >= 10 ? "\n..." : "")
-        )
+        .setDescription(`${string1}\n${next && next.length > 0 ? string2 : ''}`)
     )
 }
