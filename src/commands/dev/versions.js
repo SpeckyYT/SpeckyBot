78047004d631f4112a03f59485607f1f90f917ae
SpeckyYT <Specky.fans@gmail.com>
2020-12-23 17:07:57 +0100

    Update versions.js

--- src/commands/dev/versions.js
+++ src/commands/dev/versions.js
@@ -2,9 +2,8 @@ module.exports = {
     name: "versions",
     description: "Gives you the versions of the used software of the bot!",
     category: 'dev',
+    type: 'send',
     aliases: ['v']
 }
 
-module.exports.run = async (bot, msg) => {
-    msg.channel.send(JSON.stringify(process.versions,null,4).code('json'));
-}
+module.exports.run = async (bot, msg) => JSON.stringify(process.versions,null,2).code('json');
