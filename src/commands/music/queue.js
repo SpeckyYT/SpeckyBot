6911f9a84b5ac620e1c4b4c16310bccde7acba06
SpeckyYT <Specky.fans@gmail.com>
2020-12-25 15:51:09 +0100

    improved code for music queue

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -5,6 +5,8 @@ module.exports = {
     aliases: ["q","qu","que"]
 }
 
+const queueLength = 10;
+
 module.exports.run = async (bot, msg) => {
     if(!bot.music.isPlaying(msg.guild.id)) throw new Error('Not playing');
     const queue = await bot.music.getQueue(msg.guild.id);
@@ -12,12 +14,16 @@ module.exports.run = async (bot, msg) => {
 
     const [current, ...next] = queue.songs;
 
+    /** Now Playing */
     const string1 = `Now Playing: ${current.author} - ${current.name}\n\n`.code('css');
-    const string2 = `${next.slice(0,10).map((song,i) => `#${i+1} | ${song.author} - ${song.name}`).join('\n')}`.code('md');
+    /** Queue */
+    const string2 = next.length && next.slice(0,queueLength).map((song,i) => `#${i+1} | ${song.author} - ${song.name}`).join('\n').code('md');
+    /** Longer than queue... */
+    const string3 = next.length > queueLength && `and other ${next.length-queueLength} songs...`.code();
 
     return msg.channel.send(
         bot.embed()
         .setTitle("Queue")
-        .setDescription(`${string1}\n${next && next.length > 0 ? string2 : ''}`)
+        .setDescription([string1,string2,string3].filter(s=>s).join('\n'))
     )
 }
