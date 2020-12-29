3f2133f4c59c78971c0873f3cb45532cba9e81f0
SpeckyYT <Specky.fans@gmail.com>
2020-12-29 17:36:17 +0100

    fixed "reaction.removeAll is not a function" error

--- src/events/globalchat/reactions/add.js
+++ src/events/globalchat/reactions/add.js
@@ -5,7 +5,11 @@ module.exports = {
 module.exports.call = async (bot, reaction, user) => {
     await Promise.all(bot.cache.globalchatsent).catch(()=>{});
     if(bot.globalchats.has(reaction.message.channel.id)){
-        if(!(reaction.emoji.id === null || bot.emojis.cache.has(reaction.emoji.id))) reaction.removeAll().catch(()=>{});
+        if(
+            reaction.emoji.id !== null
+            &&
+            !bot.emojis.cache.has(reaction.emoji.id)
+        ) reaction.remove().catch(()=>{});
         if(user.bot) return;
         const am = bot.cache.globalchat.get(reaction.message.id) || bot.cache.globalchat.find(ma => ma.find(m => m.id === reaction.message.id));
         if(am){
