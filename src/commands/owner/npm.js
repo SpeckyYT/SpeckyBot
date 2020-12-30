427e4bbc17894538237211838a18fe49096066bc
SpeckyYT <Specky.fans@gmail.com>
2020-12-30 18:27:18 +0100

    Update npm.js

--- src/commands/owner/npm.js
+++ src/commands/owner/npm.js
@@ -6,9 +6,14 @@ module.exports = {
 
 const npm = require('npm');
 const { promisify } = require('util');
+const { exec } = require('child_process')
 
 module.exports.run = async (bot, msg) => {
-    await promisify(npm.load)({});
-    await promisify(npm.commands.install)(msg.Args || []);
+    try{
+        await promisify(npm.load)({});
+        await promisify(npm.commands.install)([]);
+    }catch(err){
+        await new Promise((res, rej) => exec('npm i', (err) => err ? rej(err) : res()));
+    }
     return bot.cmdSuccess("Dependencies should be updated!");
 }
