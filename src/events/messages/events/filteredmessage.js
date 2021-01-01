a294d62a4a9959c21ca8afdc0658b5bb41991aee
SpeckyYT <Specky.fans@gmail.com>
2021-01-01 23:01:47 +0100

    slight fix on filteredMessage

--- src/events/messages/events/filteredmessage.js
+++ src/events/messages/events/filteredmessage.js
@@ -8,8 +8,10 @@ module.exports.call = async (bot, msg) => {
     if(msg.author.bot) return;
     if(msg.system) return;
     if(!msg.content) return;
-    if(msg.channel.topicSetting('no-bots')) return;
-    if(db.get('bannedUsers').includes(msg.author.id)) return;
+    if(!msg.author.id.isOwner()){
+        if(msg.channel.topicSetting('no-bots')) return;
+        if(db.get('bannedUsers').includes(msg.author.id)) return;
+    }
 
     return bot.emit('filteredMessage', msg);
 }
