a1ecf61a8027601d5743d789190e8dfcfa8c65d1
SpeckyYT <Specky.fans@gmail.com>
2021-01-01 18:10:48 +0100

    merged nsfw and sfw templates

---
+++ src/templates/n-sfw.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require('discord.js');
+const { Nekos, NekoLove, Miss, HMtai, Freaker } = require('hmfull');
+const promisify = require('promisify-func');
+
+async function handle(bot, msg, methods, sfw){
+    const method = Array.isArray(methods) ? methods.pick() : methods;
+
+    const api = [Nekos, NekoLove, Miss, HMtai, Freaker]
+    .filter(api => api[sfw][method])
+    .pick();
+
+    if(!api) return bot.cmdError('Endpoint not found');
+
+    return msg.channel.send(
+        await promisify(api[sfw][method]())()
+        .then(img => new MessageEmbed().setImage(img.url))
+    )
+}
+
+module.exports.nsfw = ({methods}) =>
+    async function(bot,msg){
+        return handle(bot, msg, methods, 'nsfw');
+    }
+
+module.exports.sfw = ({methods,methodsNSFW}) =>
+    async function(bot,msg){
+        if(methodsNSFW && !msg.flag("sfw") && msg.channel.isNSFW())
+            return module.exports.nsfw({methods: methodsNSFW})(bot,msg);
+        return handle(bot, msg, methods, 'sfw');
+    }
+
