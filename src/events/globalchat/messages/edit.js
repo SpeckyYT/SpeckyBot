1e1168aaab9551349851ac33bc409e03d01813e2
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-04 18:50:47 +0000

    update inviteregex

--- src/events/globalchat/messages/edit.js
+++ src/events/globalchat/messages/edit.js
@@ -4,7 +4,7 @@ module.exports = {
 
 module.exports.call = async (bot, _, msg) => {
     const am = bot.cache.globalchat.get(msg.id);
-    const inviteRegex = /(https?:\/\/)?(www\.)?((discord\.(gg|io|me|li)|discordapp\.com\/invite|invite\.gg))(\/|\\\/|\\)[A-z+0-9]{1,}/g;
+    const inviteRegex = /(?:https?)?(?::\/\/)?(?:di?sc(?:ord(?:app)?)?|top)\.(?:com|gg|invite|net)\/+[\w/]+/gi;
     msg.content = msg.content.replace(inviteRegex,'https://discord.gg/4EecFku');
     if(am){
         am.forEach(ms => ms.edit(bot.globalChatEmbed(msg)).catch(()=>{}))
