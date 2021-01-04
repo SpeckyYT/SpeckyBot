74dc0570c766520ce2426bc835bfb461d105ae69
Chiranthan Bharadwaj R <chiranthan568@gmail.com>
2021-01-05 00:26:18 +0530

    Update edit.js

--- src/events/globalchat/messages/edit.js
+++ src/events/globalchat/messages/edit.js
@@ -4,7 +4,7 @@ module.exports = {
 
 module.exports.call = async (bot, _, msg) => {
     const am = bot.cache.globalchat.get(msg.id);
-    const inviteRegex = /(https?:\/\/)?(www\.)?((discord\.(gg|io|me|li)|discordapp\.com\/invite|invite\.gg))(\/|\\\/|\\)[A-z+0-9]{1,}/g;
+    const inviteRegex = /(https?:\/\/)?(www\.)?(((discord|dsc|top)\.(gg|io|me|li)|discordapp\.com\/invite|invite\.gg))(\/|\\\/|\\)[A-z+0-9]{1,}/g;
     msg.content = msg.content.replace(inviteRegex,'https://discord.gg/4EecFku');
     if(am){
         am.forEach(ms => ms.edit(bot.globalChatEmbed(msg)).catch(()=>{}))
