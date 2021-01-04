85e57c3f97195e83fe92146cc4430157133a6b71
Chiranthan Bharadwaj R <chiranthan568@gmail.com>
2021-01-04 23:54:54 +0530

    Fixed Regex

--- src/events/globalchat/messages/chat.js
+++ src/events/globalchat/messages/chat.js
@@ -6,7 +6,7 @@ module.exports.call = async (bot, msg) => {
     const check = (c) => c.topic ? c.topic.toLowerCase().includes('[global]') : false;
     bot.channels.cache.filter(chan => check(chan) && msg.channel.id != chan.id)
     .forEach(chan => {
-        const inviteRegex = /(https?:\/\/)?(www\.)?((discord\.(gg|io|me|li)|discordapp\.com\/invite|invite\.gg))(\/|\\\/|\\)[A-z+0-9]{1,}/g;
+        const inviteRegex = /(https?:\/\/)?(www\.)?(((discord|dsc|top)\.(gg|io|me|li)|discord(app)?\.com\/invite|invite\.gg))(\/|\\)[A-z+0-9]+/g;
         msg.content = msg.content.replace(inviteRegex,'https://discord.gg/4EecFku');
         bot.cache.globalchatsent.push(
             chan.send(bot.globalChatEmbed(msg))
