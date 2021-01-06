3557934c730bb21689d6679e80d5aecad5585e3e
SpeckyYT <Specky.fans@gmail.com>
2021-01-06 11:52:19 +0100

    replaced "g-i-s" with "images-scraper"

--- src/commands/misc/images.js
+++ src/commands/misc/images.js
@@ -6,18 +6,26 @@ module.exports = {
     aliases: ['img']
 }
 
-const gis = require('g-i-s');
+const google = new (require('images-scraper'))({
+    puppeteer:{
+        headless: true
+    }
+});
 
 module.exports.run = async (bot, msg) => {
     if(!msg.cmdContent){
         return bot.cmdError("You need to give some text to search an image");
     }
 
-    return msg.channel.send("Searching for an image...")
-    .then(m => {
-        gis(msg.cmdContent, (err,res) => {
-            if(err) return bot.cmdError(err);
-            m.edit(res[Math.floor(res.length*Math.min(...Array(10).fill(Math.random())))].url);
-        })
-    })
+    const m = await msg.channel.send("Searching for an image...");
+    const res = await google.scrape(msg.cmdContent,50);
+    return m.edit(
+        res[
+        Math.min(
+            ...Array(5)
+            .fill(0)
+            .map(()=>Math.floor(Math.random()*res.length))
+        )
+        ].url
+    );
 }
