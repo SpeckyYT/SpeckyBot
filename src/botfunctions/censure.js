1e57d523eaf62e959624efbfd21c32850d7c0861
SpeckyYT <Specky.fans@gmail.com>
2021-01-06 19:54:20 +0100

    made a better bad-words caching system

--- src/botfunctions/censure.js
+++ src/botfunctions/censure.js
@@ -1,18 +1,22 @@
 const fetch = require('node-fetch');
 
-const badwords = fetch('https://raw.githubusercontent.com/chucknorris-io/swear-words/master/en')
-.then(res => res.text())
-.then(text => text.split('\n'));
+const bwsource = 'https://raw.githubusercontent.com/chucknorris-io/swear-words/master/en';
 
 module.exports = (bot) => {
+    let { badwords } = this;
 
     bot.censureText = async (string) => {
+        if(!badwords){
+            badwords = await fetch(bwsource)
+            .then(res => res.text())
+            .then(text => text.split('\n'));
+        }
         const censures = [
             bot.emotes.censure1,
             bot.emotes.censure2
         ];
         return string.replace(
-            new RegExp(`(?<=\\b)(${(await badwords).join('|')})(?=\\b)`,'gi'),
+            new RegExp(`(?<=\\b)(${badwords.join('|')})(?=\\b)`,'gi'),
             Array(Math.ceil(Math.random()*4))
             .fill('')
             .map(()=>censures.pick())
