d0a8eafc440e16f363129ab8733f377e4d928124
SpeckyYT <Specky.fans@gmail.com>
2021-01-10 18:23:56 +0100

    added the actual emoji items in emotes

--- src/handlers/emotes.js
+++ src/handlers/emotes.js
@@ -7,10 +7,14 @@ module.exports = (bot) => {
         const values = Object.values(emojis);
         Object.keys(emojis)
         .forEach((name,index) => {
-            if(bot.emotes[name]){
-                throw new Error(`Emote ${name || 'null'} already exists`);
+            if(bot.emotes[name]) throw new Error(`Emote ${name} already exists`);
+            const emojiID = values[index].match(bot.regex.id);
+            const emoji = emojiID ? bot.emojis.cache.get(emojiID[0]) : null;
+            if(emoji){
+                bot.emotes[name] = emoji;
+            }else{
+                bot.emotes[name] = values[index];
             }
-            bot.emotes[name] = values[index];
         })
     })
 };
