c418d18792f746927c2cce2a52c79cd04d68e78a
SpeckyYT <Specky.fans@gmail.com>
2021-01-24 17:55:44 +0100

    added "deleteall" flag to calculator

--- src/commands/math/calc.js
+++ src/commands/math/calc.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "The math command to evaluate some math!",
     usage: "<problem>",
     category: "math",
-    flags: ['delete','scope'],
+    flags: ['delete','scopes','deleteall'],
     aliases: ["calculator","calculate","cal"]
 }
 
@@ -13,21 +13,25 @@ const { inspect } = require('util');
 
 module.exports.run = async (bot, msg) => {
     if(msg.flag('scopes') || msg.flag('scope')){
-        const scopes = mathscopes.get(`${msg.author.id}`) || {}
-        return msg.channel.send(JSON.stringify(scopes,null,2).code('json'))
+        const scopes = mathscopes.get(`${msg.author.id}`) || {};
+        return msg.channel.send(JSON.stringify(scopes,null,2).code('json'));
     }
     if(msg.flag('delete')){
         const scope = msg.Args[0];
         if(!scope) return bot.cmdError('Scope to delete not included');
         mathscopes.delete(`${msg.author.id}.${scope}`);
-        return bot.cmdSuccess(`Scope \`${scope}\` deleted!`)
+        return bot.cmdSuccess(`Scope \`${scope}\` deleted!`);
+    }
+    if(msg.flag('deleteall')){
+        mathscopes.delete(`${msg.author.id}`);
+        return bot.cmdSuccess('All your scopes got deleted!');
     }
 
-    if(!msg.cmdContent) return bot.cmdError('Input a calculation');
+    if(!msg.cmdContent) return bot.cmdError('No input found');
 
     const { result, error } = bot.matheval(msg.cmdContent, msg);
 
-    if(error) return bot.cmdError(error)
+    if(error) return bot.cmdError(error);
 
     return msg.channel.send(
         bot.embed()
