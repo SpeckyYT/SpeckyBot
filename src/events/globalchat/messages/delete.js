c37489df4b906da6723fd76cfe8743074bacb4b2
SpeckyYT <Specky.fans@gmail.com>
2021-01-25 20:54:37 +0100

    small globalchat fix

--- src/events/globalchat/messages/delete.js
+++ src/events/globalchat/messages/delete.js
@@ -4,6 +4,6 @@ module.exports = {
 
 module.exports.call = async (bot, msg) => {
     const am = bot.cache.globalchat.get(msg.id);
-    if(am) am.forEach(ms => ms.delete().catch(()=>{}));
+    if(am) for(const ms of am) await ms.delete().catch(()=>{});
     bot.cache.globalchat.delete(msg.id);
 }
