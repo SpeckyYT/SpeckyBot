08250212d58d9dcc810a5804d4d895c2f7679544
WickedWizard3588 <chiranthan568@gmail.com>
2021-02-03 21:30:24 +0530

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const prompts = [
+	{
+		type: 'input',
+		name: 'prefix',
+		message: 'Please enter your bot Prefix',
+	},
+	{
+		type: 'password',
+		name: 'token',
+		message: 'Please enter the bot token from the application page.',
+	},
+	{
+		type: 'number',
+		name: 'owner',
+		message: 'Please enter the Bot Owner User ID. Only one ID is accepted currently, to avoid abuse',
+	},
+];
+
+inquirer.prompt(prompts).then((answers) => {
+	const replies = (JSON.stringify(answers, null, ' '));
+	fs.appendFile('test.json', replies, function(err) {
+		if (err) {
+			throw err;
+		}
+		console.log('Created a test.json for you :)');
+	});
+});
