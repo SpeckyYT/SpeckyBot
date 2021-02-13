2a6740e18a6a18aeed16e419df0beaa8c8adfc64
SpeckyYT <Specky.fans@gmail.com>
2021-02-13 18:38:59 +0100

    filtered GC in multiple guilds

--- src/events/globalchat/load/load.js
+++ src/events/globalchat/load/load.js
@@ -3,8 +3,10 @@ module.exports = {
 }
 
 module.exports.call = (bot) => {
+    const servers = [];
     const filter = c =>
-        c.topicSetting('global');
-
+        c.topicSetting('global') &&
+        !servers.includes((c.guild||{}).id) &&
+        (servers.push((c.guild||{}).id), true);
     bot.globalchats = bot.channels.cache.filter(filter);
 }
