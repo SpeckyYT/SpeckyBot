0fd1ff14d803e54ef24a74fbca835d15045522d7
SpeckyYT <Specky.fans@gmail.com>
2021-02-21 14:17:12 +0100

    Update vote.js

--- src/commands/important/vote.js
+++ src/commands/important/vote.js
@@ -7,16 +7,31 @@ module.exports = {
 
 module.exports.run = async (bot, msg) => {
     const botlists = [
-        ['Discord Boats','https://discord.boats/bot/398157933315227649/vote'],
-        ['Discord Bot List','https://discordbotlist.com/bots/speckybot/upvote'],
-        ['Shield Bot List','https://shieldbotlist.tk/bots/like/398157933315227649'],
-        ['Discord Extreme List','https://discordextremelist.xyz/en-US/bots/398157933315227649'],
-        ['NextGen Bots','https://nextgenbots.xyz/bots/398157933315227649/vote'],
+        [
+            'Discord Boats',
+            'https://discord.boats/bot/398157933315227649/vote'
+        ],
+        [
+            'Discord Bot List',
+            'https://discordbotlist.com/bots/speckybot/upvote'
+        ],
+        [
+            'Shield Bot List',
+            'https://shieldbotlist.tk/bots/like/398157933315227649'
+        ],
+        [
+            'Discord Extreme List',
+            'https://discordextremelist.xyz/en-US/bots/398157933315227649'
+        ],
+        [
+            'NextGen Bots',
+            'https://nextgenbots.xyz/bots/398157933315227649/vote'
+        ],
     ]
 
-    const embed = bot.membed().setTitle('Upvote SpeckyBot!');
-
-    for(const botlist of botlists) embed.addField(...botlist);
+    const embed = bot.embed()
+    .setTitle('Upvote SpeckyBot!')
+    .setDescription(botlists.map(([list,link]) => `[${list}](${link})`).join('\n'));
 
     return msg.channel.send(embed);
 }
