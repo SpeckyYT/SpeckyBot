0c8033a88c2bc6762f9efadbf6ff6edacdb8f589
SpeckyYT <Specky.fans@gmail.com>
2021-02-24 22:04:49 +0100

    added http(s) to the invitelink regex

--- src/botfunctions/regex.js
+++ src/botfunctions/regex.js
@@ -10,7 +10,7 @@ module.exports = (bot) => {
         spaces: / +/g,
         id: /(\d{17,19})/g,
         messageLink: /https?:\/\/(?:\w+\.)?discord(?:app)?\.com\/channels\/(\d+)\/(\d+)\/(\d+)\/?/gi,
-        inviteLink: /(discord(?:app)?\.gg(?:\/|\\+\/+)|discord(?:app)?\.com(?:\/|\\+\/+)(?:invite\/))([A-z0-9-]{2,})/gi,
+        inviteLink: /(?:\S+)(https?:\/\/)?(www\.)?(discord(?:app)?\.gg(?:\/|\\+\/+)|discord(?:app)?\.com(?:\/|\\+\/+)(?:invite\/))([A-z0-9-]{2,})/gi,
         link: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&\\/=]*)/gi,
         emote: /(?<=<a?:[\w]+:)(\d{17,19})(?=>)/gi
     }
