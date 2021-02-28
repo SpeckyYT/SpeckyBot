529a123f6d46fa8e0bb0226146a31e92946875ae
WickedWizard3588 <chiranthan568@gmail.com>
2021-02-28 16:51:38 +0530

    Update unbanuser.js

--- src/commands/owner/unbanuser.js
+++ src/commands/owner/unbanuser.js
@@ -15,6 +15,8 @@ module.exports.run = async (bot, msg) => {
     const target = await bot.users.fetch(res[0]);
     if (!target) return bot.cmdError('Invalid User Mention/ID.');
 
+    if(bot.config.owner.includes(target.id)) return bot.cmdError('Owners cannot be Banned/Unbanned');
+
     if (!db.get('bannedUsers').includes(target.id))
         return bot.cmdError(`User ${target} is not banned.`);
 
