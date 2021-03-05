208526aa8b0798d907c7a9ebb58b96ded4fc671b
SpeckyYT <Specky.fans@gmail.com>
2021-03-05 16:40:29 +0100

    fixed stop/pause/resume commands

--- src/commands/music/pause.js
+++ src/commands/music/pause.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: "stop",
-    description: "Stops the music!",
+    name: "pause",
+    description: "Pauses the music!",
     category: "music",
     aliases: []
 }
 
 module.exports.run = async (bot, msg) => {
     if(!bot.music.isPlaying(msg.guild.id)) throw new Error('Not playing');
-    await bot.music.stop(msg.guild.id);
-    return bot.cmdSuccess('Playback stopped.');
+    await bot.music.pause(msg.guild.id);
+    return bot.cmdSuccess('Playback paused.');
 }
--- src/commands/music/resume.js
+++ src/commands/music/resume.js
@@ -6,7 +6,7 @@ module.exports = {
 }
 
 module.exports.run = async (bot, msg) => {
-    if(!bot.music.isPlaying(msg.guild.id)) throw new Error('Not playing')
+    if(!bot.music.isPlaying(msg.guild.id)) throw new Error('Not playing');
     await bot.music.resume(msg.guild.id);
     return bot.cmdSuccess('Playback resumed.');
 }
