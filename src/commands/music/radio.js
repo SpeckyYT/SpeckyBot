ef731fc8c369725a54170cb0c5510f903bbc6ed2
SpeckyYT <Specky.fans@gmail.com>
2021-03-10 21:12:56 +0100

    added more playlists to radio

--- src/commands/music/radio.js
+++ src/commands/music/radio.js
@@ -5,6 +5,7 @@ module.exports = {
 }
 
 const radios = [
+    // NORMAL MUSIC
     {
         name: 'Muse',
         url: "https://youtube.com/playlist?list=PLTpHAWOyakLaaSnO1SyJor4r0y_ZmYwHg",
@@ -13,12 +14,43 @@ const radios = [
     {
         name: 'Black Wolf',
         url: "https://youtube.com/playlist?list=PLNiLQueObdrT1TeviIQ7xJNqElO-55WRT",
-        from: 'Black Wolf'
+        from: 'Black Wolf',
     },
     {
         name: 'NCS',
         url: "https://youtube.com/playlist?list=PLzkuLC6Yvumv_Rd5apfPRWEcjf9b1JRnq",
-        from: 'Specky'
+        from: 'Specky',
+    },
+    {
+        name: 'Trap Nation',
+        url: "https://youtube.com/playlist?list=PLC1og_v3eb4hrv4wsqG1G5dsNZh9bIscJ",
+        from: 'Melvin',
+    },
+
+    // VARIOUS
+    {
+        name: 'osu!',
+        url: "https://youtube.com/playlist?list=PLt2s8p17wbVywg7bINV9nj7GPG5Oj0B4W",
+        from: 'Specky',
+    },
+
+    // CLASSICAL MUSIC
+    {
+        name: 'BombSquad',
+        url: "https://youtube.com/playlist?list=PLmjn_XKBcplZObP4r1jksIIr5nmO4dANf",
+        from: 'Specky',
+    },
+    {
+        name: 'RCT',
+        url: "https://youtube.com/playlist?list=PLxt6cj1N5f6A3Ag0NF5Ew1l0QdcfMyckB",
+        from: 'Specky',
+    },
+
+    // GAMES
+    {
+        name: 'Celeste',
+        url: "https://youtube.com/playlist?list=PL1eFjFaZ9VkyDcVnvJyEC3P8tCFpZpRoU",
+        from: 'Specky',
     },
 ];
 
