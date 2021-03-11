f0304b4f68c35c2d024cc6e9c1516bb82b7c81a6
SpeckyYT <Specky.fans@gmail.com>
2021-03-11 20:36:24 +0100

    Update globalChatMessage.js

--- src/events/log/globalChatMessage.js
+++ src/events/log/globalChatMessage.js
@@ -7,8 +7,14 @@ module.exports.call = async (bot, msg) => {
     if(!msg.channel.topicSetting('global')) return;
     const channel = bot.channels.cache.get("813019518141333525");
     if(!channel) return;
+    const data = [
+        `Message: ${msg.id}`,
+        `Author: ${msg.author.id}`,
+        `Guild: ${msg.guild.id}`,
+        `is Bot: ${msg.author.bot}`
+    ]
     if(!msg.author.bot || !msg.embeds.length)
-        return channel.send(bot.globalChatEmbed(msg));
+        return channel.send(data, bot.globalChatEmbed(msg));
     if(msg.embeds.length)
-        return channel.send(msg.embeds[0]);
+        return channel.send(data, msg.embeds[0]);
 }
