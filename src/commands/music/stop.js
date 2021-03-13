f9d9a45fac77ef0158384513ba8a96acc5cb351a
SpeckyYT <Specky.fans@gmail.com>
2021-03-13 15:03:37 +0100

    removed unnecessary await

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -7,6 +7,6 @@ module.exports = {
 
 module.exports.run = async (bot, msg) => {
     if(!bot.music.isPlaying(msg)) throw new Error('Not playing');
-    await bot.music.stop(msg);
+    bot.music.stop(msg);
     return bot.cmdSuccess('Playback stopped.');
 }
