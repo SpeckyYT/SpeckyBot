4eddec54ab3b795c199d110101de0e090a628d69
SpeckyYT <Specky.fans@gmail.com>
2021-03-14 11:09:56 +0100

    fixed globalchat permissions

--- src/botfunctions/permissions.js
+++ src/botfunctions/permissions.js
@@ -2,10 +2,17 @@ const send = 'SEND_MESSAGES';
 const view = 'VIEW_CHANNEL';
 const files = 'ATTACH_FILES';
 const embed = 'EMBED_LINKS';
+const msgs = 'MANAGE_MESSAGES';
 
 module.exports = (bot) => {
     bot.perms = {
-        globalchat: [view,send,files,embed],
+        globalchat: [
+            view,
+            send,
+            files,
+            embed,
+            msgs,
+        ],
         commands: [files,embed],
     }
 }
