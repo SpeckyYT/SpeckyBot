ab2768b7cd71c84c83e1370cbe906f6109ff1942
SpeckyYT <Specky.fans@gmail.com>
2021-03-23 20:09:37 +0100

    extended commands and events to arrays

--- src/handlers/events.js
+++ src/handlers/events.js
@@ -9,37 +9,41 @@ module.exports = (bot) => {
     bot.cache.schedules = [];
 
     global.modules.loader(bot, 'events', ({filePath}) => {
-        const evt = bot.require(filePath);
-        let eName = evt.event;
-        if(!eName) throw new Error("Event not found!".toUpperCase());
-        const calltype = evt.type == "once" ? "once" : "on";
-        const emi = evt.emitter || 'bot';
-        if(!Array.isArray(eName)) eName = [eName];
-        for(let event of eName){
-            let emitter;
-            switch(emi){
-                case 'process':
-                    emitter = process; break;
-                case 'bot':
-                    emitter = bot; break;
-            }
-            if(!(emitter instanceof EventEmitter)){
-                throw new Error("Event Emitter not found!");
-            }
-            if(validate(event)){
-                if(!emitter.eventNames().includes(event)){
-                    bot.cache.schedules.push(
-                        schedule(
-                            event,
-                            () => emitter.emit(event),
-                            {
-                                timezone: evt.timezone
-                            }
-                        )
-                    );
+        const data = bot.require(filePath);
+        const evts = Array.isArray(data) ? data : [data];
+
+        for(const evt of evts){
+            let eName = evt.event;
+            if(!eName) throw new Error("Event not found!".toUpperCase());
+            const calltype = evt.type == "once" ? "once" : "on";
+            const emi = evt.emitter || 'bot';
+            if(!Array.isArray(eName)) eName = [eName];
+            for(let event of eName){
+                let emitter;
+                switch(emi){
+                    case 'process':
+                        emitter = process; break;
+                    case 'bot':
+                        emitter = bot; break;
                 }
+                if(!(emitter instanceof EventEmitter)){
+                    throw new Error("Event Emitter not found!");
+                }
+                if(validate(event)){
+                    if(!emitter.eventNames().includes(event)){
+                        bot.cache.schedules.push(
+                            schedule(
+                                event,
+                                () => emitter.emit(event),
+                                {
+                                    timezone: evt.timezone
+                                }
+                            )
+                        );
+                    }
+                }
+                emitter[calltype](event, promisify(bot.getFunction(evt).bind(null, bot)));
             }
-            emitter[calltype](event, promisify(bot.getFunction(evt).bind(null, bot)));
         }
     })
 };
