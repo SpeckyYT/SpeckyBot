a722635a04662403181d865f73d7440df1b5d9fb
SpeckyYT <Specky.fans@gmail.com>
2021-03-23 11:11:01 +0100

    Update get.js

--- src/botfunctions/get.js
+++ src/botfunctions/get.js
@@ -1,13 +1,10 @@
 module.exports = (bot) => {
-    bot.getCommand = (command) => {
-        return bot.commands.get(command) || bot.commands.get(bot.aliases.get(command));
-    }
+    bot.getCommand = (command) =>
+        bot.commands.get(command) || bot.commands.get(bot.aliases.get(command));
 
-    bot.getConsoleCommand = (command) => {
-        return bot.console.get(command) || bot.console.get(bot.consoleali.get(command));
-    }
+    bot.getConsoleCommand = (command) =>
+        bot.console.get(command) || bot.console.get(bot.consoleali.get(command));
 
-    bot.getFunction = (cmd) => {
-        return (typeof cmd == 'object') ? cmd[Object.keys(cmd).filter(v => typeof cmd[v] == 'function')[0]] : async ()=>{};
-    }
+    bot.getFunction = (cmd) =>
+        (typeof cmd == 'object') ? cmd[Object.keys(cmd).filter(v => typeof cmd[v] == 'function')[0]] : async ()=>{};
 }
