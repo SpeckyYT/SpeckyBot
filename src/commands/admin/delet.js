36a539b4c5f8563091575ab8bbad0be7d0b9e3d8
Devonte <69095599+devnote-dev@users.noreply.github.com>
2021-04-15 18:28:40 +0100

    Update delet.js

--- src/commands/admin/delet.js
+++ src/commands/admin/delet.js
@@ -3,8 +3,8 @@ module.exports = {
     description: "Deletes the last 3 messages!",
     category: "admin",
     aliases: ["..","."],
-    userPerms: ['MANAGE_MESSAGES'],
-    botPerms: ['MANAGE_MESSAGES']
+    userPerms: 8192,
+    botPerms: 8192
 }
 
 module.exports.run = (bot, msg) => msg.channel.bulkDelete(3);
