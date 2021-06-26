518ebf126c249bb1f0f162fe34fa93fc347ac83f
Devonte <69095599+devnote-dev@users.noreply.github.com>
2021-06-26 05:15:20 +0100

    feat: bigints

--- src/events/messages/messages.js
+++ src/events/messages/messages.js
@@ -6,8 +6,8 @@ module.exports.call = async (bot, msg) => {
     return; // probably this will do something soon™
 
     // MESSAGES/RESPONSES
-    if(msg.guild.me.permissions.has(2048)){}
+    if(msg.guild.me.permissions.has(2048n)){}
 
     // OTHER
-    if(msg.guild.me.permissions.has(8192)){}
+    if(msg.guild.me.permissions.has(8192n)){}
 }
